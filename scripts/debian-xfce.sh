fdad5e86241b779ba7e8955d21d7917a4cf9e870
xelser <dkzenzuri@gmail.com>
2022-03-21 17:21:18 +0800

    Updated @ debian

--- scripts/debian-xfce.sh
+++ scripts/debian-xfce.sh
@@ -3,38 +3,42 @@ clear
 
 ############################### Preparation ##############################
 
-# user var
-user="xelser"
-home="/home/${user}"
+# Add user to sudo
+if [ $UID -eq 1000 ]; then
+	echo "Run this command: 'usermod -aG sudo $USER'"
+	su -l root
+else
+	echo "DONT RUN THIS WITH ROOT"
+	exit 1
+fi
 
 # dotfiles
 echo && read -p "Copy (xelser's) dotfiles? (y/N): " cp_dotfiles
 case $cp_dotfiles in
    y)	# Remove old .config files
-   	rm -rf ${home}/.config
+   	rm -rf $HOME/.config
    	cd /tmp/ && git clone https://github.com/xelser/dotfiles
-   	cp -rf /tmp/dotfiles/fedora-workstation/.config ${home};;
+   	cp -rf /tmp/dotfiles/fedora-workstation/.config $HOME;;
    *)	;;
 esac
 
+clear
 ################################ Packages ################################
 
 # Debloat
-apt autoremove --purge -y libreoffice*
+sudo apt autoremove --purge -y libreoffice* xterm
 
 # Update
-apt update && apt upgrade -y && apt full-upgrade -y
+sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y
 
 # Install
-apt install -y lightdm-gtk-greeter-settings gvfs-{backends,fuse} unrar zip wget curl numlockx \
+sudo apt install -y lightdm-gtk-greeter-settings gvfs-{backends,fuse} unrar zip wget curl numlockx \
   htop neofetch gparted transmission gnome-{disk-utility,builder} plank pulseeffects \
   flatpak gtk2-engines-murrine gtk2-engines-pixbuf fonts-noto mtools exfatprogs
 
 # Flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-su - ${user} << EOF
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub -y com.github.tchx84.Flatseal com.bitwarden.desktop com.discordapp.Discord
-EOF
 
 clear
 ################################ Configs #################################
@@ -43,31 +47,40 @@ clear
 echo "
 [Seat:*]
 greeter-setup-script=/usr/bin/numlockx on
-autologin-user=${user}" | tee -a /etc/lightdm/lightdm.conf
+autologin-user=$USER" | sudo tee -a /etc/lightdm/lightdm.conf
 
-# Add user to sudo
-usermod -aG sudo ${user}
+# lightdm-gtk-greeter
+echo "
+[greeter]
+theme-name = Matcha-dark-aliz
+xft-hintstyle = hintslight
+icon-theme-name = Papirus-Dark
+font-name = Noto Sans 10
+xft-dpi = 96
+hide-user-image = true
+clock-format = %a, %I:%M %p
+indicators = ~host;~spacer;~clock;~spacer;~power" | sudo tee -a /etc/lightdm/lightdm-gtk-greeter.conf
 
 # Bash configs
-rm -rf ${home}/{.profile,.bashrc}
-cp /etc/skel/{.profile,.bashrc} ${home}/
-cat $HOME/distro-scripts/configs/bash/debian_bashrc >> ${home}/.bashrc
+rm -rf $HOME/{.profile,.bashrc}
+cp /etc/skel/{.profile,.bashrc} $HOME/
+cat $HOME/distro-scripts/configs/bash/debian_bashrc >> $HOME/.bashrc
 
 # Font rendering
 cp -rf $HOME/distro-scripts/configs/x11-font-rendering/local.conf /etc/fonts/
-cp -rf $HOME/distro-scripts/configs/x11-font-rendering/.Xresources ${home}/
-xrdb -merge ${home}/.Xresources
-ln -sf /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
-ln -sf /usr/share/fontconfig/conf.avail/10-hinting-slight.conf /etc/fonts/conf.d/
-ln -sf /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d/
-fc-cache -fv
+cp -rf $HOME/distro-scripts/configs/x11-font-rendering/.Xresources $HOME/
+xrdb -merge $HOME/.Xresources
+sudo ln -sf /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
+sudo ln -sf /usr/share/fontconfig/conf.avail/10-hinting-slight.conf /etc/fonts/conf.d/
+sudo ln -sf /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d/
+sudo fc-cache -fv
 
 clear
 ################################# Themes #################################
 
 # GTK
 cd /tmp/ && git clone https://github.com/vinceliuice/Matcha-gtk-theme.git
-./Matcha-gtk-theme/install.sh -c dark -t aliz
+sudo ./Matcha-gtk-theme/install.sh -c dark -t aliz
 
 # Icons
 wget -qO- https://git.io/papirus-icon-theme-install | sh
@@ -75,14 +88,14 @@ wget -qO- https://git.io/papirus-folders-install | sh
 papirus-folders -C red --theme Papirus-Dark
 
 # Cursor
-apt install -y breeze-cursor-theme
+sudo apt install -y breeze-cursor-theme
 
 clear
 ############################## Housekeeping ##############################
 
 # Clean packages
-apt autoremove --purge -y && apt autoclean
+sudo apt autoremove --purge -y && sudo apt autoclean
 flatpak uninstall --unused -y
 
 # Change owner
-chown -R ${user} ${home}
+sudo chown -R $USER $HOME
