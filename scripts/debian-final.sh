0ff055059651a75c3e0c59363827a60ac3721f6a
xelser <dkzenzuri@gmail.com>
2022-04-07 18:13:00 +0800

    Updated @ debian

--- scripts/debian-final.sh
+++ scripts/debian-final.sh
@@ -6,19 +6,16 @@ notify-send "Finalizing Installation" "Please be patient, this may take a while.
 
 ################################ Flatpaks ################################
 
+# Permissions
+flatpak --user override --filesystem=/usr/share/icons/:ro
+flatpak --user override --filesystem=$HOME/.local/share/icons:ro
+flatpak --user override --filesystem=$HOME/.icons/:ro
+
 # Install
 flatpak install flathub -y com.github.tchx84.Flatseal org.x.Warpinator com.bitwarden.desktop com.discordapp.Discord com.skype.Client us.zoom.Zoom
 
 # Clean Packages
 flatpak uninstall --unused -y
-
-# Flatpak: Permissions
-flatpak --user override com.github.tchx84.Flatseal --filesystem=/usr/share/icons/:ro
-flatpak --user override org.x.Warpinator --filesystem=/usr/share/icons/:ro
-flatpak --user override com.bitwarden.desktop --filesystem=/usr/share/icons/:ro
-flatpak --user override com.discordapp.Discord --filesystem=/usr/share/icons/:ro
-flatpak --user override com.skype.Client --filesystem=/usr/share/icons/:ro
-flatpak --user override us.zoom.Zoom --filesystem=/usr/share/icons/:ro
 
 clear
 ############################## Housekeeping ##############################
