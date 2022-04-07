0070d3abe5b2bf62a85c26e161eab00dd6b49d38
xelser <dkzenzuri@gmail.com>
2022-04-07 19:49:43 +0800

    Updated @ debian

--- scripts/debian-final.sh
+++ scripts/debian-final.sh
@@ -7,9 +7,9 @@ notify-send "Finalizing Installation" "Please be patient, this may take a while.
 ################################ Flatpaks ################################
 
 # Permissions
-flatpak --user override --filesystem=/usr/share/icons/:ro
-flatpak --user override --filesystem=$HOME/.local/share/icons:ro
-flatpak --user override --filesystem=$HOME/.icons/:ro
+#flatpak --user override --filesystem=/usr/share/icons/:ro
+#flatpak --user override --filesystem=$HOME/.local/share/icons:ro
+#flatpak --user override --filesystem=$HOME/.icons/:ro
 
 # Install
 flatpak install flathub -y com.github.tchx84.Flatseal org.x.Warpinator com.bitwarden.desktop com.discordapp.Discord com.skype.Client us.zoom.Zoom
