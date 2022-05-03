79c88b2c32011ba60888f050d60e19b0f96b8e7e
xelser <dkzenzuri@gmail.com>
2022-05-04 00:28:48 +0800

    Updated @ debian

--- scripts/manjaro-kde.sh
+++ scripts/manjaro-kde.sh
@@ -31,18 +31,13 @@ yay -S --needed --noconfirm --disable-download-timeout --cleanafter --removemake
 # Gaming
 yay -S --needed --noconfirm --disable-download-timeout \
   steam gamemode lib32-gamemode mangohud lib32-mangohud goverlay-bin optimus-manager optimus-manager-qt lutris bottles \
+  nvidia-dkms nvidia-utils lib32-nvidia-utils mesa lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-radeon lib32-vulkan-radeon \
   wine-mono wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
   mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error \
   lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
   sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
   ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 \
   lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
-
-# Drivers
-yay -S --needed --noconfirm --disable-download-timeout \
-  nvidia-dkms nvidia-utils  lib32-nvidia-utils   vulkan-icd-loader lib32-vulkan-icd-loader \
-  lib32-mesa  vulkan-intel  lib32-vulkan-intel   vulkan-icd-loader lib32-vulkan-icd-loader \
-  lib32-mesa  vulkan-radeon lib32-vulkan-radeon  vulkan-icd-loader lib32-vulkan-icd-loader
 
 # Dependencies
 sudo pacman -S --asdeps --noconfirm sassc
