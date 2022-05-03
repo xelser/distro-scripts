dd7bb1afcb46fee056b2294fe390c7325965d66d
Axel <81435783+xelser@users.noreply.github.com>
2022-05-03 21:58:51 +0800

    Update manjaro-kde.sh

--- scripts/manjaro-kde.sh
+++ scripts/manjaro-kde.sh
@@ -30,8 +30,8 @@ yay -S --needed --noconfirm --disable-download-timeout --cleanafter --removemake
  
 # Gaming
 yay -S --needed --noconfirm --disable-download-timeout \
-  steam gamemode lib32-gamemode mangohud lib32-mangohud goverlay-bin optimus-manager optimus-manager-qt \
-  lutris wine-mono wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
+  steam gamemode lib32-gamemode mangohud lib32-mangohud goverlay-bin optimus-manager optimus-manager-qt lutris bottles \
+  wine-mono wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
   mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error \
   lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
   sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
