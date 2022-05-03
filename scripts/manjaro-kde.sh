65094d94e7cea706ec7982ee1f9a7bf10fa89d9b
Axel <81435783+xelser@users.noreply.github.com>
2022-05-04 02:26:33 +0800

    Update manjaro-kde.sh

--- scripts/manjaro-kde.sh
+++ scripts/manjaro-kde.sh
@@ -31,7 +31,7 @@ yay -S --needed --noconfirm --disable-download-timeout --cleanafter --removemake
 # Gaming
 yay -S --needed --noconfirm --disable-download-timeout \
   steam gamemode lib32-gamemode mangohud lib32-mangohud goverlay-bin optimus-manager optimus-manager-qt lutris bottles \
-  nvidia-dkms nvidia-utils lib32-nvidia-utils mesa lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-radeon lib32-vulkan-radeon \
+  nvidia-dkms nvidia-utils lib32-nvidia-utils mesa lib32-mesa vkd3d lib32-vkd3d vulkan-intel lib32-vulkan-intel vulkan-radeon lib32-vulkan-radeon \
   wine-mono wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
   mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error \
   lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
