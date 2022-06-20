7b289c71ef1fe7fa632545b5653ce8cba9a20abb
Axel <81435783+xelser@users.noreply.github.com>
2022-06-20 16:28:41 +0800

    Update manjaro-cinnamon.sh

--- scripts/manjaro-cinnamon.sh
+++ scripts/manjaro-cinnamon.sh
@@ -33,9 +33,8 @@ sudo pacman-mirrors --geoip && sudo pacman -Syyu --noconfirm --needed --disable-
 
 # Gaming
 sudo pacman -S --noconfirm --needed --disable-download-timeout \
-  steam nvidia-utils lib32-nvidia-utils gamemode lib32-gamemode mangohud-x11 lib32-mangohud-x11 \
-  mesa lib32-mesa vkd3d lib32-vkd3d vulkan-intel lib32-vulkan-intel vulkan-radeon lib32-vulkan-radeon \
-  wine-mono wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
+  steam gamemode lib32-gamemode mangohud lib32-mangohud wine-mono \
+  wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
   mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error \
   lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo \
   sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama \
