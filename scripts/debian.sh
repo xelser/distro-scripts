e708f136b870fc372f7572245bc65410a5bb243c
xelser <dkzenzuri@gmail.com>
2023-08-23 21:40:11 +0800

    auto-update @ arch

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -10,18 +10,18 @@ sed -i 's/non-free non-free/non-free/g' /etc/apt/sources.list
 apt update && apt install nala --yes
 
 # INSTALL: Debian Base (x11) 
-nala install --assume-yes --no-install-recommends build-essential curl \
+nala install --assume-yes firefox-esr build-essential curl \
+  plymouth qt5ct qt5-style-kvantum lxappearance fonts-ubuntu{,-console} \
   pipewire-{alsa,audio,jack,pulse} wireplumber easyeffects lsp-plugins-lv2 \
-  plymouth-themes qt5ct qt5-style-kvantum lxappearance fonts-ubuntu{,-console} \
   lightdm-gtk-greeter-settings light-locker mugshot numlockx dconf-{editor,cli} \
-  {redshift,transmission}-gtk firefox-esr geany
+  {redshift,transmission}-gtk geany
 
 # INSTALL: Debian XFCE 
-nala install --assume-yes --no-install-recommends xfce4{,-goodies,-notifyd,-power-manager} \
+nala install --assume-yes xfce4{,-goodies,-notifyd,-power-manager} \
   gvfs-{fuse,backends} thunar-volman parole
 
 # INSTALL: Debian i3
-nala install --assume-yes --no-install-recommends brightnessctl i3-wm picom polybar nitrogen \
+nala install --assume-yes brightnessctl i3-wm picom polybar nitrogen \
   alacritty neovim xclip ranger imv mpv rofi dunst
 
 #################################### CONFIG ####################################
@@ -34,9 +34,6 @@ sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
 sed -i 's/quiet/quiet splash/g' /etc/default/grub
 sed -i 's/splash splash/splash/g' /etc/default/grub
 update-grub
-
-# plymouth
-plymouth-set-default-theme -R bgrt 
 
 # lightdm
 echo -e "[Seat:*]
