3dbba8503c0737f0ee3c4c6f9a9c8f358e417d56
xelser <dkzenzuri@gmail.com>
2023-08-23 13:52:25 +0800

    auto-update @ arch

--- scripts/debian-post.sh
+++ scripts/debian-post.sh
@@ -3,7 +3,6 @@
 #################################### THEMES ####################################
 
 # gtk, icons and more
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/themes/theme-matcha.sh)"
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/themes/pack-catppuccin.sh)"
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/themes/fonts-nerd.sh UbuntuMono)"
 
--- scripts/debian.sh
+++ scripts/debian.sh
@@ -9,14 +9,19 @@ sed -i 's/non-free non-free/non-free/g' /etc/apt/sources.list
 # PACKAGE MANAGER: Nala
 apt update && apt install nala --yes
 
+# INSTALL: Debian Base (x11) 
+nala install --assume-yes --no-install-recommend xorg sudo build-essential curl \
+  lightdm-gtk-greeter-settings light-locker mugshot numlockx dconf-{editor,cli} \
+  plymouth qt5ct qt5-style-kvantum lxappearance fonts-ubuntu{,-console} \
+  {redshift,transmission}-gtk firefox-esr geany
+
 # INSTALL: Debian XFCE 
-nala install --assume-yes --no-install-recommends build-essential curl firefox-esr \
-	plymouth qt5ct qt5-style-kvantum lxappearance ukui-wallpapers fonts-ubuntu{,-console} \
-	lightdm-gtk-greeter-settings mugshot dconf-{editor,cli} numlockx 
+nala install --assume-yes --no-install-recommends xfce4{,goodies,notifyd,power-manager} \
+  gvfs-{fuse,backends} thunar-volman parole
 
 # INSTALL: Debian i3
 nala install --assume-yes --no-install-recommends brightnessctl i3-wm picom polybar nitrogen \
-	alacritty ranger imv mpv gammastep rofi dunst libnotify4 neovim xclip wl-clipboard 
+  alacritty ranger imv mpv gammastep rofi dunst libnotify4 neovim xclip wl-clipboard 
 
 #################################### CONFIG ####################################
 
@@ -28,4 +33,12 @@ sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
 sed -i 's/quiet/quiet splash/g' /etc/default/grub
 sed -i 's/splash splash/splash/g' /etc/default/grub
 update-grub
+
+# lightdm
+echo -e "[Seat:*]
+autologin-user=xelser
+autologin-user-timeout=0
+greeter-hide-users=false
+" >> /etc/lightdm/lightdm.conf
+systemctl enable lightdm
 
