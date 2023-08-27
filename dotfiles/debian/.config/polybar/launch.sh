e473c94286b1fa6c23b37d3b7dc0ae7c23655708
xelser <dkzenzuri@gmail.com>
2023-08-27 10:32:32 +0800

    auto-update @ debian

--- dotfiles/debian/.config/polybar/launch.sh
+++ dotfiles/debian/.config/polybar/launch.sh
@@ -6,6 +6,5 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar default 2>&1 | tee -a /tmp/polybar.log & disown
 
-echo "Polybar launched..."
+polybar default-${machine} || polybar default 2>&1 | tee -a /tmp/polybar.log & disown
