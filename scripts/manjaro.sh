78148754c18c2c90dc1bf231b17e505c6fe0a232
xelser <dkzenzuri@gmail.com>
2023-08-30 01:54:31 +0800

    auto-update @ debian

--- scripts/manjaro.sh
+++ scripts/manjaro.sh
@@ -8,12 +8,14 @@ sudo pacman-mirrors --country Global
 
 # DEBLOAT/UPDATE/INSTALL: Manjaro Base
 sudo pacman -Rnsc --noconfirm manjaro-hello midori
-sudo pacman -Syyu --needed --noconfirm plymouth-theme-manjaro ttf-noto-nerd noto-fonts qt5ct kvantum power-profiles-daemon mhwd \
-	dconf-editor vlc redshift warpinator plank geany transmission-gtk sassc 
-
+sudo pacman -Syyu --needed --noconfirm plymouth-theme-manjaro ttf-noto-nerd noto-fonts qt5ct kvantum \
+	power-profiles-daemon mhwd dconf-editor redshift warpinator geany transmission-gtk firefox
+	
 # INSTALL: Manjaro DE
-if [[ ${wm_de} == "cinnamon" ]]; then
-	sudo pacman -S --needed --noconfirm x{reader,viewer} firefox jack2 pulseaudio-equalizer-ladspa
+if [[ ${wm_de} == "xfce" ]]; then sudo pacman -S --needed --noconfirm \
+	pulseaudio-equalizer-ladspa 
+elif [[ ${wm_de} == "cinnamon" ]]; then sudo pacman -S --needed --noconfirm \
+	x{reader,viewer} jack2 pulseaudio-equalizer-ladspa plank vlc
 fi
 
 #################################### CONFIG ####################################
@@ -24,27 +26,6 @@ sudo e2label /dev/${partition} "Manjaro"
 
 # grub
 sudo sed -i 's/quiet/quiet splash/g' /etc/default/grub
-sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=20/g' /etc/default/grub
+sudo sed -i 's/splash splash/splash/g' /etc/default/grub
+sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
-
-# lightdm-slick-greeter
-echo "[Greeter]
-background=/usr/share/backgrounds/greeter_default.jpg
-theme-name=vimix-dark-compact-beryl
-icon-theme-name=Vimix-beryl-dark
-cursor-theme-name=Vimix-white-cursors
-activate-numlock=false
-draw-user-backgrounds=false
-clock-format=%I:%M %p
-" | sudo tee /etc/lightdm/slick-greeter.conf 1> /dev/null
-
-#################################### THEMES ####################################
-
-# INSTALL: GTK, KDE, Icon, Cursors
-case $in_themes in
-   y)	${source_dir}/themes/theme-vimix.sh
-	${source_dir}/themes/icon-vimix.sh
-	${source_dir}/themes/cursor-vimix.sh
-	;;
-   *)	;;
-esac
