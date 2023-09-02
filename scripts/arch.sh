ed57f8cb6d033995025168fd5e7c3b00421e9761
xelser <dkzenzuri@gmail.com>
2023-09-02 14:18:49 +0800

    auto-update @ arch

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -5,7 +5,7 @@ install_i3 () {
 arch-chroot /mnt /bin/bash << EOF
 
 # Packages
-pacman -S --needed --noconfirm xdg-user-dirs brightnessctl nm-connection-editor gvfs-{afc,goa,google,gphoto2,mtp,nfs,smb} \
+pacman -S --needed --noconfirm xdg-user-dirs brightnessctl numlockx nm-connection-editor gvfs-{afc,goa,google,gphoto2,mtp,nfs,smb} \
   sddm firefox alacritty ranger imv mpv gammastep rofi wallutils swaybg feh dunst libnotify neovim{,-plugins} xclip wl-clipboard \
   pipewire-{alsa,audio,jack,pulse,zeroconf} easyeffects wireplumber lsp-plugins-lv2 ecasound flameshot xdg-desktop-portal-wlr grim \
   grub-customizer qt5ct kvantum lxappearance-gtk3 ttf-fira{-sans,code-nerd} picom i3-wm polybar sway waybar \
@@ -17,7 +17,7 @@ sed -i 's/plymouth plymouth/plymouth/g' /etc/mkinitcpio.conf
  
 # sddm
 echo -e "[Autologin]\nUser=${user}\nSession=i3" >> /etc/sddm.conf
-#echo -e "\n[General]\nNumlock=on" >> /etc/sddm.conf
+echo -e "\n[General]\nNumlock=on" >> /etc/sddm.conf
 systemctl enable sddm
 
 EOF
