984f06f4f9f3d5b4ef87ff054f401a58c9633e7a
xelser <dkzenzuri@gmail.com>
2023-09-06 12:49:49 +0800

    auto-update @ arch

--- modules/flatpak.sh
+++ modules/flatpak.sh
@@ -8,19 +8,19 @@ elif [ -f /usr/bin/apt ]; then sudo apt install --yes flatpak
 fi
 
 # Packages 
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak remote-modify --enable flathub && flatpak install --assumeyes --noninteractive flathub com.github.tchx84.Flatseal
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && sudo flatpak remote-modify --enable flathub
+flatpak install --assumeyes --noninteractive flathub com.github.tchx84.Flatseal
 
 # Configs
-flatpak override --filesystem=/usr/share/icons/:ro
-flatpak override --filesystem=/usr/share/themes/:ro
-flatpak override --filesystem=xdg-config/gtk-3.0
-flatpak override --filesystem=xdg-config/gtk-4.0
+sudo flatpak override --filesystem=/usr/share/icons/:ro
+sudo flatpak override --filesystem=/usr/share/themes/:ro
+sudo flatpak override --filesystem=xdg-config/gtk-3.0
+sudo flatpak override --filesystem=xdg-config/gtk-4.0
 
 # QT/kvantum
 if [ -f /usr/bin/kvantummanager ]; then
 	flatpak install --assumeyes --noninteractive flathub org.kde.KStyle.Kvantum/x86_64/5.15-22.08
-	flatpak override --filesystem=/usr/share/Kvantum/:ro
-	flatpak override --filesystem=xdg-config/Kvantum:ro
-	flatpak override --env=QT_STYLE_OVERRIDE=kvantum-dark
+	sudo flatpak override --filesystem=/usr/share/Kvantum/:ro
+	sudo flatpak override --filesystem=xdg-config/Kvantum:ro
+	sudo flatpak override --env=QT_STYLE_OVERRIDE=kvantum-dark
 fi
