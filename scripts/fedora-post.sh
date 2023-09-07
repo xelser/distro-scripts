9f5645c264b1e3477bbb6b140fad475490d256cd
xelser <dkzenzuri@gmail.com>
2023-09-07 20:58:21 +0800

    auto-update @ arch

--- scripts/fedora-post.sh
+++ scripts/fedora-post.sh
@@ -17,7 +17,6 @@ sudo dnf autoremove --assumeyes gnome-terminal
 #################################### FLATPAK ###################################
 
 # INSTALL: Fedora Workstation
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/modules/flatpak.sh)"
 flatpak install --assumeyes --noninteractive flathub org.gtk.Gtk3theme.adw-{gtk3,gtk3-dark} com.mattjakeman.ExtensionManager \
   com.rafaelmardojai.Blanket me.dusansimic.DynamicWallpaper io.bassi.Amberol org.x.Warpinator \
   us.zoom.Zoom com.google.Chrome com.spotify.Client
