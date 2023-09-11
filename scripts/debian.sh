a12942ef1b6036460f3a025632aa6157b211050d
xelser <dkzenzuri@gmail.com>
2023-09-11 14:16:45 +0800

    auto-update @ arch

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -1,46 +1,45 @@
 #!/bin/bash
 
-append_file () {
-        grep -x "$1" $2 || echo -e "\n$1" | sudo tee -a $2 1> /dev/null
-}
-
 ################################### PACKAGES ###################################
 
 # Debian Non-Free Repos
-sudo sed -i 's/non-free-firmware/non-free-firmware non-free/g' /etc/apt/sources.list
-sudo sed -i 's/non-free non-free/non-free/g' /etc/apt/sources.list
+sed -i 's/non-free-firmware/non-free-firmware non-free/g' /etc/apt/sources.list
+sed -i 's/non-free non-free/non-free/g' /etc/apt/sources.list
 
 # PACKAGE MANAGER: Nala
-sudo apt update && sudo apt install nala --yes
+apt update && apt install nala --yes
 
 # INSTALL: Debian Base
-sudo nala install --assume-yes lightdm build-essential curl firefox-esr \
+nala install --assume-yes lightdm build-essential curl firefox-esr \
         qt5ct qt5-style-kvantum blueman mugshot pulseeffects numlockx \
         dconf-{editor,cli} redshift transmission-gtk geany
 
-if [[ ${wm_de} == "tty" ]]; then sudo nala install --assume-yes \
+if [[ ${wm_de} == "tty" ]]; then nala install --assume-yes \
         i3-wm brightnessctl picom polybar nitrogen alacritty \
         neovim xclip dunst libnotify4 ranger imv mpv rofi \
         xarchiver pcmanfm lxappearance 
-elif [[ ${wm_de} == "xfce" ]]; then sudo nala install --assume-yes \
+elif [[ ${wm_de} == "xfce" ]]; then nala install --assume-yes \
         lightdm-gtk-greeter-settings gvfs-{backends,fuse} 
 fi
 
 #################################### CONFIG ####################################
 
+# sudo for user
+usermod -aG sudo ${user}
+
 # grub 
-sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
-sudo sed -i 's/quiet/quiet splash/g' /etc/default/grub
-sudo sed -i 's/splash splash/splash/g' /etc/default/grub
-sudo update-grub
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
+sed -i 's/quiet/quiet splash/g' /etc/default/grub
+sed -i 's/splash splash/splash/g' /etc/default/grub
+update-grub
 
 # lightdm
-append_file "[Seat:*]
+echo -e "\n[Seat:*]
 autologin-user=xelser
 autologin-user-timeout=0
-greeter-hide-users=false
-#user-session=xfce" /etc/lightdm/lightdm.conf
-sudo systemctl enable lightdm
+greeter-hide-users=false" >> /etc/lightdm/lightdm.conf
+[[ ${wm_de} == "xfce" ]] && echo -e "user-session=xfce" >> /etc/lightdm/lightdm.conf
+systemctl enable lightdm
 
 #################################### THEMES ####################################
 
@@ -50,3 +49,4 @@ if [ ! -f /.flag ]; then
 	${source_dir}/themes/icon-papirus.sh
         ${source_dir}/themes/fonts-nerd.sh Noto
 fi
+
