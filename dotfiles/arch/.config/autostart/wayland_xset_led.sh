619ce4423cce271db9314842b14f33273e72303e
xelser <dkzenzuri@gmail.com>
2023-09-17 00:06:01 +0800

    auto-update @ arch

--- dotfiles/arch/.config/autostart/wayland_xset_led.sh
+++ dotfiles/arch/.config/autostart/wayland_xset_led.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 while true; do
-	input_num="$(ls /sys/class/leds/input*::scrolllock/brightness | cut -d':' -f1)"
-	echo 1 | sudo tee "${input_num}::scrolllock/brightness" 1>/dev/null
+	brightnessctl -d input2::scrolllock s 1
 done
--- scripts/debian-post.sh
+++ scripts/debian-post.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [[ ${wm_de} == "xfce" ]]; then 
+#if [[ ${wm_de} == "xfce" ]]; then 
         # Themes and Fonts
-        xfconf-query -cn xsettings -pn /Net/ThemeName -t string -s "Matcha-dark-aliz"
-        xfconf-query -cn xsettings -pn /Net/IconThemeName -t string -s "Papirus-Dark"
-        xfconf-query -cn xsettings -pn /Gtk/CursorThemeName -t string -s "Adwaita"
-        xfconf-query -cn xsettings -pn /Gtk/FontName -t string -s "Noto Sans 10"
-        xfconf-query -cn xsettings -pn /Gtk/MonospaceFontName -t string -s "Noto Mono 10"
-        xfconf-query -cn parole -pn /subtitles/font -t string -s "Noto Mono Bold 10"
-        gsettings set org.xfce.mousepad.preferences.view color-scheme 'matchav2'
-        gsettings set org.xfce.mousepad.preferences.view font-name 'Noto Mono Bold 10'        
-fi
+        #xfconf-query -cn xsettings -pn /Net/ThemeName -t string -s "Matcha-dark-aliz"
+        #xfconf-query -cn xsettings -pn /Net/IconThemeName -t string -s "Papirus-Dark"
+        #xfconf-query -cn xsettings -pn /Gtk/CursorThemeName -t string -s "Adwaita"
+        #xfconf-query -cn xsettings -pn /Gtk/FontName -t string -s "Noto Sans 10"
+        #xfconf-query -cn xsettings -pn /Gtk/MonospaceFontName -t string -s "Noto Mono 10"
+        #xfconf-query -cn parole -pn /subtitles/font -t string -s "Noto Mono Bold 10"
+        #gsettings set org.xfce.mousepad.preferences.view color-scheme 'matchav2'
+        #gsettings set org.xfce.mousepad.preferences.view font-name 'Noto Mono Bold 10'        
+#fi
 
