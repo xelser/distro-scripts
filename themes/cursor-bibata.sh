4cfa87a7e9af267400dd5f43502c495201aa21a0
xelser <dkzenzuri@gmail.com>
2023-09-21 14:14:30 +0800

    auto-update @ arch

--- themes/cursor-bibata.sh
+++ themes/cursor-bibata.sh
@@ -2,7 +2,6 @@
 
 version="$(curl --silent "https://api.github.com/repos/ful1e5/Bibata_Cursor/releases/latest" | grep tag_name | cut -d'"' -f4)"
 
-default () {
 # Clean
 cd /tmp/ && rm -rf /tmp/Bibata*
 sudo rm -rf /usr/share/icons/Bibata*
@@ -15,13 +14,8 @@ wget -q https://github.com/ful1e5/Bibata_Cursor/releases/download/${version}/Bib
 cd /usr/share/icons/
 sudo tar -xf /tmp/Bibata-Modern-Classic.tar.gz
 sudo tar -xf /tmp/Bibata-Modern-Ice.tar.gz
-}
 
-fedora () {
-        
-sudo dnf copr enable peterwu/rendezvous --assumeyes
-sudo dnf install bibata-cursor-themes --assumeyes
-        
-}
-
-[[ ${distro_id} == "fedora" ]] && fedora || default
+#sudo dnf copr enable peterwu/rendezvous --assumeyes
+#sudo dnf install bibata-cursor-themes --assumeyes
+#[[ ${distro_id} == "fedora" ]] && fedora || default
+#
