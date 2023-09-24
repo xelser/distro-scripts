e85ebe12350069a9050b6ccd96d33b1b413b685b
xelser <dkzenzuri@gmail.com>
2023-09-24 09:38:08 +0800

    auto-update @ debian

--- scripts/debian-post.sh
+++ scripts/debian-post.sh
@@ -15,7 +15,7 @@ if [[ ${wm_de} == "xfce" ]]; then
         xfconf-query -cn xsettings -pn /Net/ThemeName -t string -s "Gruvbox-Material-Dark"
         xfconf-query -cn xsettings -pn /Net/IconThemeName -t string -s "Papirus-Dark"
         xfconf-query -cn xsettings -pn /Gtk/CursorThemeName -t string -s "phinger-cursors"
-        xfconf-query -cn xsettings -pn /Gtk/CursorThemeSize -t int -s "32"
+        xfconf-query -cn xsettings -pn /Gtk/CursorThemeSize -t int -s "24"
         xfconf-query -cn xsettings -pn /Gtk/FontName -t string -s "Noto Sans 10"
         xfconf-query -cn xsettings -pn /Gtk/MonospaceFontName -t string -s "NotoMono Nerd Font 10"
         xfconf-query -cn xfwm4 -pn /general/theme -t string -s "Gruvbox-Material-Dark"
