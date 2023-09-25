c2b3d75b0efd7e1c315dd4bfccdd08d41fc5dbae
xelser <dkzenzuri@gmail.com>
2023-09-25 22:56:06 +0800

    auto-update @ debian

--- dotfiles/debian/.config/autostart/autostart-script.sh
+++ dotfiles/debian/.config/autostart/autostart-script.sh
@@ -5,13 +5,3 @@ xfconf-query -cn thunar -pn /last-show-hidden -t bool -s "false"
 xfconf-query -cn thunar -pn /last-window-height -t int -s "600"
 xfconf-query -cn thunar -pn /last-window-width -t int -s "900"   
 
-# Keyboard and Numlock
-xset led on
-#numlockx on
-
-# Wallpaper
-nitrogen --restore
-
-# Polybar
-sh $HOME/.config/polybar/launch.sh && sleep 10s && \
-	xdo hide -N Polybar && xdo show -N Polybar
