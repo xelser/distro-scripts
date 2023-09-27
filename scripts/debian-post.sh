9c5c7d129274319994b7bbd4d8c5d1a52879048f
xelser <dkzenzuri@gmail.com>
2023-09-27 10:58:42 +0800

    auto-update @ debian

--- scripts/debian-post.sh
+++ scripts/debian-post.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Gruvbox Material
+# gruvbox material
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/themes/pack-gruvbox.sh)"
 
-# Vim/Neovim Plug
+# vim/neovim plug
 [ -f /usr/bin/vim ] && curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
@@ -24,3 +24,14 @@ if [[ ${wm_de} == "xfce" ]]; then
         dconf write /org/xfce/mousepad/preferences/view/color-scheme "'gruvbox_material_hard_dark'"
         dconf write /org/xfce/mousepad/preferences/view/font-name "'UbuntuMono Nerd Font 11.5'"
 fi
+
+# rofi (launcher and powermenu)
+cd /tmp/ && git clone --depth=1 https://github.com/xelser/rofi.git && cd rofi && chmod +x setup.sh && ./setup.sh && cd
+
+sed -i 's/style-1/style-3/g' $HOME/.config/rofi/launchers/type-4/launcher.sh
+sed -i 's/onedark/gruvbox-material-hard-dark/g' $HOME/.config/rofi/launchers/type-4/shared/colors.rasi
+sed -i 's/Iosevka/UbuntuMono/g' $HOME/.config/rofi/launchers/type-4/shared/fonts.rasi
+
+sed -i 's/style-1/style-5/g' $HOME/.config/rofi/powermenu/type-1/powermenu.sh
+sed -i 's/onedark/gruvbox-material-hard-dark/g' $HOME/.config/rofi/powermenu/type-1/shared/colors.rasi
+sed -i 's/JetBrains Mono/UbuntuMono/g' $HOME/.config/rofi/powermenu/type-1/shared/fonts.rasi
