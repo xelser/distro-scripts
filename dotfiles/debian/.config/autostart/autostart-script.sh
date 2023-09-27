6c03010f61c5134a87b3f10317a96cc19dd8cf31
xelser <dkzenzuri@gmail.com>
2023-09-27 11:09:59 +0800

    auto-update @ debian

--- dotfiles/debian/.config/autostart/autostart-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# XFCE Settings
-xfconf-query -cn thunar -pn /last-show-hidden -t bool -s "false"
-xfconf-query -cn thunar -pn /last-window-height -t int -s "600"
-xfconf-query -cn thunar -pn /last-window-width -t int -s "900"   
-
