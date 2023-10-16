d9e4266051e8a112912507ad5836ef964afb2bea
xelser <dkzenzuri@gmail.com>
2023-10-16 13:48:29 +0800

    auto-update @ debian

--- dotfiles/debian/.local/share/light-mode.d/default.sh
+++ dotfiles/debian/.local/share/light-mode.d/default.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+# rofi
+sed -i 's/dark/light/g' $HOME/.config/rofi/launchers/type-4/shared/colors.rasi
+sed -i 's/dark/light/g' $HOME/.config/rofi/powermenu/type-1/shared/colors.rasi
+
+# gtksourceview
+dconf write /org/xfce/mousepad/preferences/view/color-scheme "'gruvbox-material-hard-light'"
+
 # xsettingsd
-cat xsettingsd > $HOME/.xsettingsd
+cat $HOME/.local/share/light-mode.d/xsettingsd > $HOME/.xsettingsd
+killall -HUP xsettingsd
+
+# nitrogen
+nitrogen --set-zoom-fill /usr/share/backgrounds/gruvbox/cyber-girl-light.png --save
+
+# polybar
+cat $HOME/.config/polybar/themes/gruvbox-material-hard-light.ini > $HOME/.config/polybar/current_theme.ini
+pkill -USR1 polybar
