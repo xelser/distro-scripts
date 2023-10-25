5af93223a6943e39710636bc160501c3c00d729f
xelser <dkzenzuri@gmail.com>
2023-10-25 18:38:11 +0800

    auto-update @ debian

--- scripts/debian-post.sh
+++ scripts/debian-post.sh
@@ -2,7 +2,7 @@
 
 # themes and fonts
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/themes/pack-gruvbox.sh)"
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/themes/fonts-nerd.sh FiraCode)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/themes/fonts-nerd.sh UbuntuMono)"
 
 # vim/neovim plug
 [ -f /usr/bin/vim ] && curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -12,17 +12,17 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/ma
         --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
        
 # Themes and Fonts
-[ -f /usr/bin/mousepad ] && dconf write /org/xfce/mousepad/preferences/view/font-name "'FiraCode Nerd Font 10'"
+[ -f /usr/bin/gedit ] && dconf write /org/gnome/gedit/preferences/editor/editor-font "'UbuntuMono Nerd Font 10.5'"
 
 if [[ ${wm_de} == "xfce" ]]; then 
         xfconf-query -cn xsettings -pn /Net/ThemeName -t string -s "Gruvbox-Material-Dark"
         xfconf-query -cn xsettings -pn /Net/IconThemeName -t string -s "Papirus-Dark"
         xfconf-query -cn xsettings -pn /Gtk/CursorThemeName -t string -s "phinger-cursors"
         xfconf-query -cn xsettings -pn /Gtk/CursorThemeSize -t int -s "24"
-        xfconf-query -cn xsettings -pn /Gtk/FontName -t string -s "Fira Sans 10"
-        xfconf-query -cn xsettings -pn /Gtk/MonospaceFontName -t string -s "FiraCode Nerd Font 10"
+        xfconf-query -cn xsettings -pn /Gtk/FontName -t string -s "Ubuntu 10"
+        xfconf-query -cn xsettings -pn /Gtk/MonospaceFontName -t string -s "UbuntuMono Nerd Font 10.5"
         xfconf-query -cn xfwm4 -pn /general/theme -t string -s "Gruvbox-Material-Dark"
-        xfconf-query -cn xfwm4 -pn /general/title_font -t string -s "Fira Sans Bold 9"
+        xfconf-query -cn xfwm4 -pn /general/title_font -t string -s "Ubuntu Bold 9"
 fi
 
 # rofi (launcher and powermenu)
@@ -30,11 +30,11 @@ cd /tmp/ && git clone --depth=1 https://github.com/xelser/rofi.git && cd rofi &&
 
 sed -i 's/style-1/style-3/g' $HOME/.config/rofi/launchers/type-4/launcher.sh
 sed -i 's/onedark/gruvbox-material-hard-dark/g' $HOME/.config/rofi/launchers/type-4/shared/colors.rasi
-sed -i 's/Iosevka/FiraCode/g' $HOME/.config/rofi/launchers/type-4/shared/fonts.rasi
+sed -i 's/Iosevka Nerd Font 10/UbuntuMono Nerd Font 10.5/g' $HOME/.config/rofi/launchers/type-4/shared/fonts.rasi
 
 sed -i 's/style-1/style-5/g' $HOME/.config/rofi/powermenu/type-1/powermenu.sh
 sed -i 's/onedark/gruvbox-material-hard-dark/g' $HOME/.config/rofi/powermenu/type-1/shared/colors.rasi
-sed -i 's/JetBrains Mono/FiraCode/g' $HOME/.config/rofi/powermenu/type-1/shared/fonts.rasi
+sed -i 's/JetBrains Mono Nerd Font 10/UbuntuMono Nerd Font 10.5/g' $HOME/.config/rofi/powermenu/type-1/shared/fonts.rasi
 
 # user systemd daemons
 systemctl enable --user mpd
