b2d7db6cbfd5ceb2e3cf6d255df0c042fe807979
xelser <dkzenzuri@gmail.com>
2023-11-02 23:23:25 +0800

    auto-update @ fedora

--- scripts/endeavouros-post.sh
+++ scripts/endeavouros-post.sh
@@ -1,8 +1,1 @@
 #!/bin/bash
-
-if [[ ${wm_de} == "gnome" ]]; then
-	flatpak install --user --assumeyes --noninteractive flathub org.gtk.Gtk3theme.adw-{gtk3,gtk3-dark} \
-	  com.mattjakeman.ExtensionManager me.dusansimic.DynamicWallpaper io.bassi.Amberol \
-	  # com.google.Chrome com.discordapp.Discord org.telegram.desktop org.gnome.gitlab.YaLTeR.VideoTrimmer
-	  # org.nickvision.tubeconverter com.rafaelmardojai.Blanket
-fi
--- scripts/endeavouros.sh
+++ scripts/endeavouros.sh
@@ -1,38 +1,14 @@
 #!/bin/bash
 
-append_file () {
-        grep -x "$1" $2 || echo -e "\n$1" | sudo tee -a $2 1> /dev/null
-}
-
 ################################### PACKAGES ###################################
 
 # INSTALL: Endeavour Base
 reflector && yay -Syyu --needed --noconfirm --removemake --cleanafter --norebuild --noredownload --batchinstall --combinedupgrade --save \
-	easyeffects dconf-editor sassc 
+	base-devel easyeffects dconf-editor sassc ttf-fira{code-nerd,-sans}
 
-if [[ ${XDG_SESSION_TYPE} == "X11" ]]; then
-	yay -S --needed --noconfirm redshift 
-fi
-
-if [[ ! ${wm_de} == "gnome" ]] && [[ ! ${wm_de} == "kde" ]]; then
-	yay -S --needed --noconfirm geany transmission-gtk vlc darkman redshift blueman plank qt5ct kvantum
-fi
-
-# INSTALL: Endeavour DE
-if [[ ${wm_de} == "gnome" ]]; then
-	yay -Rnsu --noconfirm xterm
-	yay -S --needed --noconfirm gnome-{builder,multi-writer,software,system-monitor} fragments celluloid ttf-roboto{,-mono} \
-		gnome-shell-extension-{pop-shell,nightthemeswitcher}
-
-	# INSTALL: GNOME Shell Extensions
-	bash ${source_dir}/modules/gext.sh
-elif [[ ${wm_de} == "cinnamon" ]]; then
-	yay -Rnsu --noconfirm gthumb xterm
-	yay -S --needed --noconfirm lightdm-settings xviewer-plugins
-elif [[ ${wm_de} == "xfce" ]]; then
-	yay -S --needed --noconfirm lightdm-gtk-greeter-settings light-locker mugshot \
-		polybar zscroll-git xdo nitrogen flameshot
-fi
+# INSTALL: Endeavour Budgie
+yay -S --needed --noconfirm lightdm lightdm-settings budgie gnome-terminal \
+	nautilus sushi file-roller evince gedit eog totem network-manager-applet
 
 # INSTALL: Bluetooth
 if [[ $(sudo dmesg | grep -q 'Bluetooth') -eq 0 ]]; then
@@ -53,23 +29,17 @@ sudo sed -i 's/GRUB_DEFAULT=0/GRUB_DEFAULT=saved/g' /etc/default/grub
 sudo sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/g' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
+# lightdm
+echo -e "\n[Seat:*]
+greeter-hide-users=false
+autologin-user=${user}
+" | sudo tee -a /etc/lightdm/lightdm.conf
+sudo systemctl enable lightdm
+
 #################################### THEMES ####################################
 
 # INSTALL: GTK, KDE, Icon, Cursors
-case $in_themes in
-   y)	if [[ ${wm_de} == "gnome" ]]; then
-     		${source_dir}/themes/pack-libadwaita.sh
-				${source_dir}/themes/icon-tela-circle.sh
-				${source_dir}/themes/cursor-bibata.sh
-			elif [[ ${wm_de} == "cinnamon" ]]; then
-				${source_dir}/themes/theme-vimix.sh
-				${source_dir}/themes/icon-vimix.sh
-				${source_dir}/themes/cursor-vimix.sh
-			fi
-			;;
-   *)	;;
-esac
-
-
-
-
+if [ ! -f /.flag ]; then
+	${source_dir}/themes/theme-orchis.sh
+	${source_dir}/themes/icon-papirus.sh
+fi
