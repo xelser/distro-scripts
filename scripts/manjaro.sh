657f20ff9cae2d37dda2d79dc5c798da53f86744
xelser <dkzenzuri@gmail.com>
2023-11-03 11:42:06 +0800

    auto-update @ fedora

--- scripts/manjaro.sh
+++ scripts/manjaro.sh
@@ -6,22 +6,21 @@
 echo -e "\n[options]\nVerbosePkgLists\nParallelDownloads = 5\nDisableDownloadTimeout\nILoveCandy\nColor" | sudo tee -a /etc/pacman.conf 1> /dev/null
 sudo pacman-mirrors --country Global
 
-# DEBLOAT/UPDATE/INSTALL: Manjaro Base
-sudo pacman -Rnsc --noconfirm manjaro-hello zsh
-sudo pacman -Syyu --needed --noconfirm plymouth-theme-manjaro mhwd firefox \
-	plymouth base-devel easyeffects qt5ct kvantum dconf-editor power-profiles-daemon gvfs sassc wget ttf-fira{code-nerd,-sans}
-	
+# COMMON: Debloat
+sudo pacman -Rnsc --noconfirm manjaro-{hello,settings-manager} timeshift zsh
+
 # INSTALL: Manjaro DE
-if [[ ${wm_de} == "xfce" ]]; then sudo pacman -S --needed --noconfirm \
-	pulseaudio-equalizer-ladspa 
-elif [[ ${wm_de} == "cinnamon" ]]; then sudo pacman -S --needed --noconfirm \
-	x{reader,viewer} jack2 pulseaudio-equalizer-ladspa plank vlc
-elif [[ ${wm_de} == "gnome" ]]; then sudo pacman -S --needed --noconfirm \
-	gnome-{builder,console,extensions-app,multi-writer,tweaks} \
-	file-roller easyeffects fragments celluloid drawing
-elif [[ ${wm_de} == "budgie" ]]; then sudo pacman -S --needed --noconfirm \
-	file-roller
+# NOTE:(Debloat here because manjaro isnt consistent with their OOTB packages
+if [[ ${wm_de} == "xfce" ]]; then
+	sudo pacman -S --needed --noconfirm pulseaudio-equalizer-ladspa
+elif [[ ${wm_de} == "budgie" ]]; then
+	sudo pacman -Rnsc --noconfirm lshw hexchat gthumb gufw imagewriter
+	sudo pacman -Syyu --needed --noconfirm file-roller
 fi
+
+# INSTALL: Manjaro Base
+sudo pacman -S --needed --noconfirm mhwd firefox plymouth-theme-manjaro plymouth base-devel \
+	easyeffects qt5ct kvantum dconf-editor power-profiles-daemon gvfs sassc wget ttf-fira{code-nerd,-sans}
 
 #################################### CONFIG ####################################
 
@@ -34,3 +33,11 @@ sudo sed -i 's/quiet/quiet splash/g' /etc/default/grub
 sudo sed -i 's/splash splash/splash/g' /etc/default/grub
 sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+#################################### THEMES ####################################
+
+# INSTALL: GTK, KDE, Icon, Cursors
+if [ ! -f /.flag ]; then
+	${source_dir}/themes/theme-orchis.sh
+	${source_dir}/themes/icon-papirus.sh
+fi
