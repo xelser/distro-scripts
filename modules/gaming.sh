1fe2cbdbb7c86255233109e41567551cb0635756
xelser <dkzenzuri@gmail.com>
2023-11-07 23:57:27 +0800

    auto-update @ fedora

--- modules/gaming.sh
+++ modules/gaming.sh
@@ -6,7 +6,7 @@
 install_nvidia () {
 	if [[ ${distro_id} == "fedora" ]]; then
 		echo -e "blacklist nouveau\noptions nouveau modeset=0" | sudo tee /usr/lib/modprobe.d/blacklist-nouveau.conf 1> /dev/null
-		sudo dnf install --assumeyes --allowerasing akmod-nvidia #&& sudo dracut --force
+		sudo dnf install --assumeyes --allowerasing akmod-nvidia && sudo dracut --force
 	elif [[ ${distro_id} == "arch" ]] || [[ ${distro_id} == "endeavouros" ]]; then
 		sudo pacman -S --needed --noconfirm nvidia nvidia-utils lib32-nvidia-utils
 	fi
