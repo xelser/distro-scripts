333c272e1a5d18ad02df6760d30beee076096b47
xelser <dkzenzuri@gmail.com>
2023-11-08 11:34:54 +0800

    auto-update @ fedora

--- dotfiles/fedora/.config/autostart/autostart-script.sh
+++ dotfiles/fedora/.config/autostart/autostart-script.sh
@@ -2,5 +2,5 @@
 
 # Reset Window Settings
 gsettings set org.gtk.gtk4.Settings.FileChooser show-hidden 'false'
-gsettings set org.gnome.nautilus.window-state initial-size '(880, 580)'
+gsettings set org.gnome.nautilus.window-state initial-size '(790, 580)'
 gsettings set org.gnome.Console last-window-size '(740, 520)'
