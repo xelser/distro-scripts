08e926acbfca9a8f55bb658da53f69c04e7456cf
xelser <dkzenzuri@gmail.com>
2023-11-11 14:09:35 +0800

    auto-update @ fedora

--- modules/refind.sh
+++ modules/refind.sh
@@ -3,26 +3,28 @@
 # Version
 version="$(curl -qsL "https://sourceforge.net/projects/refind/best_release.json" | sed "s/, /,\n/g" | sed -rn "/release/,/\}/{ /filename/{ 0,//s/([^0-9]*)([0-9\.]+)([^0-9]*.*)/\2/ p }}")"
 
+# Get packages
+[ -f /usr/bin/pacman ] && sudo pacman -S --needed --noconfirm shim refind sbsigntools openssl
+[ -f /usr/bin/nala ] && sudo nala install --assume-yes shim-signed refind sbsigntool openssl
+[ -f /usr/bin/dnf ] && sudo dnf install --assumeyes shim rEFInd sbsigntools openssl
+
+# Install rEFInd
+sudo refind-install --shim /boot/efi/EFI/${distro_id}/shimx64.efi --localkeys
+
+# Make Root Read/Write and Enable Boot Splash
+head -1 /boot/refind_linux.conf | grep -qw "rw" || sudo sed -i '1 s/ro /rw /' /boot/refind_linux.conf
+head -1 /boot/refind_linux.conf | grep -qw "quiet splash" || sudo sed -i '1 s/rw /rw quiet splash /' /boot/refind_linux.conf
+
+# Install rEFInd theme regular
+echo -e '\n\n2\n\n' | sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/bobafetthotmail/refind-theme-regular/master/install.sh)"
+
+## DEPRECATED ##
 # Check whether the system supports UEFI
-sudo dmesg | grep -q "EFI v"; if [ $? -eq 0 ]; then
-	if [ ! -f /usr/bin/refind-install ]; then echo Installing refind...
-		if [ -f /usr/bin/pacman ]; then
-			sudo pacman -S --needed --noconfirm --disable-download-timeout refind
-			sudo refind-install --yes
-		elif [ -f /usr/bin/dnf ]; then
-			sudo dnf list --installed | grep -q "refind"; if [ $? -ne 0 ]; then
-			sudo dnf install --assumeyes https://nchc.dl.sourceforge.net/project/refind/${version}/refind-${version}-1.x86_64.rpm; fi
-		elif [ -f /usr/bin/apt ]; then
-			sudo apt list --installed | grep -q "refind"; if [ $? -ne 0 ]; then cd /tmp/
-			wget -q https://nchc.dl.sourceforge.net/project/refind/${version}/refind_${version}-1_amd64.deb
-			sudo apt install -y /tmp/refind_${version}-1_amd64.deb; fi
-		fi
-	fi
-
-	# Make Root Read/Write and Enable Boot Splash
-	head -1 /boot/refind_linux.conf | grep -qw "rw" || sudo sed -i '1 s/ro /rw /' /boot/refind_linux.conf
-        head -1 /boot/refind_linux.conf | grep -qw "quiet splash" || sudo sed -i '1 s/rw /rw quiet splash /' /boot/refind_linux.conf
-
-        # Install rEFInd theme regular
-        echo -e '\n\n2\n\n' | sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/bobafetthotmail/refind-theme-regular/master/install.sh)"
-fi
+#if [ ! -f /usr/bin/refind-install ]; then echo Installing refind...
+#	[ -f /usr/bin/pacman ] && sudo pacman -S --needed --noconfirm refind && sudo refind-install --yes
+#	[ -f /usr/bin/dnf ] && sudo dnf list --installed | grep -q "refind" || \
+#		sudo dnf install --assumeyes https://nchc.dl.sourceforge.net/project/refind/${version}/refind-${version}-1.x86_64.rpm
+#	[ -f /usr/bin/apt ] && sudo apt list --installed | grep -q "refind" || cd /tmp/ ; \
+#		wget -q https://nchc.dl.sourceforge.net/project/refind/${version}/refind_${version}-1_amd64.deb ; \
+#		sudo apt install -y /tmp/refind_${version}-1_amd64.deb
+#fi
