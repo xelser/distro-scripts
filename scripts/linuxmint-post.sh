50f96312287c2a88d20a633b9ee342f50c6f4781
xelser <dkzenzuri@gmail.com>
2023-11-13 22:40:13 +0800

    auto-update @ debian

--- scripts/linuxmint-post.sh
+++ scripts/linuxmint-post.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Set Monospaced font
+gsettings set org.gnome.desktop.interface monospace-font-name "UbuntuMono Nerd Font 11.5"
+
 # Remove Warpinator Folder
 rm -rf $HOME/Warpinator/
