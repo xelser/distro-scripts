7cfb80ab492eb3506b01849459b06a6a383f43f2
xelser <dkzenzuri@gmail.com>
2023-11-14 19:48:44 +0800

    auto-update @ fedora

--- modules/htpdate.sh
+++ modules/htpdate.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # DEPENDENCIES
-[ -f /usr/bin/pacman ] && sudo pacman -S --needed --noconfirm make
-[ -f /usr/bin/nala ] && sudo nala install --assume-yes make
-[ -f /usr/bin/dnf ] && sudo dnf install --assumeyes make
+[ -f /usr/bin/pacman ] && sudo pacman -S --needed --noconfirm make gcc
+[ -f /usr/bin/nala ] && sudo nala install --assume-yes make gcc
+[ -f /usr/bin/dnf ] && sudo dnf install --assumeyes make gcc
 
 # BUILD
 cd /tmp && git clone https://github.com/twekkel/htpdate.git
 cd htpdate && make && sudo make install
-
-# ENABLE
-sudo systemctl enable htpdate --now
