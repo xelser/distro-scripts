8b5880372ea6f55d44f290a1323b338a1bca5dd1
xelser <dkzenzuri@gmail.com>
2023-11-14 23:20:22 +0800

    auto-update @ debian

--- scripts/manjaro-post.sh
+++ scripts/manjaro-post.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Darkman
-systemctl enable --user --now darkman
+xfconf-query -cn xfwm4 -pn /Gtk/FontName -t string -s "Fira Sans 10"
+xfconf-query -cn xfwm4 -pn /Gtk/MonospaceFontName -t string -s "FiraCode Nerd Font 10"
