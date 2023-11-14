4cee06292df8bd8772ba5c79f19c82b7e2ef03b3
xelser <dkzenzuri@gmail.com>
2023-11-14 23:20:22 +0800

    auto-update @ debian

--- scripts/manjaro.sh
+++ scripts/manjaro.sh
@@ -7,12 +7,13 @@ echo -e "\n[options]\nVerbosePkgLists\nParallelDownloads = 5\nDisableDownloadTim
 sudo pacman-mirrors --country Global
 
 # DEBLOAT
-bloat=(manjaro-{hello,settings-manager} zsh midori gufw timeshift lshw hexchat gthumb gufw imagewriter)
+bloat=(manjaro-{hello,pulse,settings-manager} pulseaudio zsh midori gufw timeshift lshw hexchat gthumb gufw imagewriter)
 for pkgs in "${bloat[@]}"; do sudo pacman -Qq ${pkgs} && sudo pacman -Rnsc --noconfirm ${pkgs}; done
 
 # INSTALL: Manjaro Base
 sudo pacman -Syyu --needed --noconfirm mhwd firefox plymouth-theme-manjaro plymouth base-devel \
-	qt5ct kvantum dconf-editor power-profiles-daemon darkman gvfs ttf-fira{code-nerd,-sans}
+	manjaro-pipewire wireplumber qt5ct kvantum dconf-editor power-profiles-daemon darkman gvfs \
+	ttf-fira{code-nerd,-sans}
 
 ################################### CONFIG ###################################
 
@@ -26,10 +27,3 @@ sudo sed -i 's/splash splash/splash/g' /etc/default/grub
 sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-################################### THEMES ###################################
-
-# INSTALL: GTK, KDE, Icon, Cursors
-#if [ ! -f /.flag ]; then
-#	${source_dir}/themes/theme-orchis.sh
-#	${source_dir}/themes/icon-papirus.sh
-#fi
