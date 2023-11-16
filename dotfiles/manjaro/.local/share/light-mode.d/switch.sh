711f7c2d70bcceaa9fba1970d070ec86c6e2c1f9
xelser <dkzenzuri@gmail.com>
2023-11-16 12:22:16 +0800

    auto-update @ manjaro

--- dotfiles/manjaro/.local/share/light-mode.d/switch.sh
+++ dotfiles/manjaro/.local/share/light-mode.d/switch.sh
@@ -9,5 +9,5 @@ cp -rf $HOME/.local/share/light-mode.d/qt5ct.conf	$HOME/.config/qt5ct/
 # xfce
 xfconf-query -cn xfwm4 -pn /general/theme -t string -s "Matcha-light-sea"
 xfconf-query -cn xsettings -pn /Net/ThemeName -t string -s "Matcha-light-sea"
-xfconf-query -cn xsettings -pn /Net/IconThemeName -t string -s "Papirus-light-Maia"
+xfconf-query -cn xsettings -pn /Net/IconThemeName -t string -s "Papirus-Light-Maia"
 xfconf-query -cn xsettings -pn /Gtk/CursorThemeName -t string -s "xcursor-breeze"
