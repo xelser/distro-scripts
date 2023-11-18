185aee1982d9c3f2dc701e237a88413bded0f735
xelser <dkzenzuri@gmail.com>
2023-11-18 13:39:43 +0800

    auto-update @ debian

--- modules/theming.sh
+++ modules/theming.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # GTK 3, Icon, Cursor, Sans, Mono
-if [[ ${wm_de} == "budgie" ]]; then
-	gtk_theme="$(gsettings get org.gnome.desktop.interface gtk-theme | cut -d"'" -f2)"
-	icon_theme="$(gsettings get org.gnome.desktop.interface icon-theme | cut -d"'" -f2)"
-	cursor_theme="$(gsettings get org.gnome.desktop.interface cursor-theme | cut -d"'" -f2)"
-	sans_font="$(gsettings get org.gnome.desktop.interface font-name | cut -d"'" -f2)"
-	mono_font="$(gsettings get org.gnome.desktop.interface monospace-font-name | cut -d"'" -f2)"
+if [[ ${wm_de} == "xfce" ]]; then
+	gtk_theme="$(xfconf-query -c xsettings -p /Net/ThemeName -v)"
+	icon_theme="$(xfconf-query -c xsettings -p /Net/IconThemeName -v)"
+	cursor_theme="$(xfconf-query -c xsettings -p /Gtk/CursorThemeName -v)"
+	sans_font="$(xfconf-query -c xsettings -p /Gtk/FontName -v)"
+	mono_font="$(xfconf-query -c xsettings -p /Gtk/MonospaceFontName -v)"
 elif [[ ${wm_de} == "cinnamon" ]]; then
 	gtk_theme="$(gsettings get org.cinnamon.desktop.interface gtk-theme | cut -d"'" -f2)"
 	icon_theme="$(gsettings get org.cinnamon.desktop.interface icon-theme | cut -d"'" -f2)"
 	cursor_theme="$(gsettings get org.cinnamon.desktop.interface cursor-theme | cut -d"'" -f2)"
 	sans_font="$(gsettings get org.cinnamon.desktop.interface font-name | cut -d"'" -f2)"
 	mono_font="$(gsettings get org.gnome.desktop.interface monospace-font-name | cut -d"'" -f2)"
-elif [[ ${wm_de} == "xfce" ]]; then
-	gtk_theme="$(xfconf-query -c xsettings -p /Net/ThemeName -v)"
-	icon_theme="$(xfconf-query -c xsettings -p /Net/IconThemeName -v)"
-	cursor_theme="$(xfconf-query -c xsettings -p /Gtk/CursorThemeName -v)"
-	sans_font="$(xfconf-query -c xsettings -p /Gtk/FontName -v)"
-	mono_font="$(xfconf-query -c xsettings -p /Gtk/MonospaceFontName -v)"
-else
+elif [[ ${wm_de} == "gnome" ]]; then
+	gtk_theme="$(gsettings get org.gnome.desktop.interface gtk-theme | cut -d"'" -f2)"
+	icon_theme="$(gsettings get org.gnome.desktop.interface icon-theme | cut -d"'" -f2)"
+	cursor_theme="$(gsettings get org.gnome.desktop.interface cursor-theme | cut -d"'" -f2)"
+	sans_font="$(gsettings get org.gnome.desktop.interface font-name | cut -d"'" -f2)"
+	mono_font="$(gsettings get org.gnome.desktop.interface monospace-font-name | cut -d"'" -f2)"
+elif [[ -f $HOME/.config/gtk-3.0/settings.ini ]]; then
 	gtk_theme="$(cat $HOME/.config/gtk-3.0/settings.ini | grep 'gtk-theme-name' | cut -d'=' -f2)"
 	icon_theme="$(cat $HOME/.config/gtk-3.0/settings.ini | grep 'gtk-icon-theme-name' | cut -d'=' -f2)"
 	cursor_theme="$(cat $HOME/.config/gtk-3.0/settings.ini | grep 'gtk-cursor-theme-name' | cut -d'=' -f2)"
@@ -43,15 +43,15 @@ if [[ ! ${wm_de} == "gnome" ]]; then
 
 	flatpak override --user --filesystem=xdg-data/themes:ro
 	flatpak override --user --env=GTK_THEME=${gtk_theme}
-
-	# GTK 4
-	rm -rf 					   "$HOME/.config/gtk-4.0/{assets,gtk.css,gtk-dark.css}"
-	mkdir -p 				   "$HOME/.config/gtk-4.0"
-	ln -sf "${theme_dir}/gtk-4.0/assets"       "$HOME/.config/gtk-4.0/"
-	ln -sf "${theme_dir}/gtk-4.0/gtk.css"      "$HOME/.config/gtk-4.0/gtk.css"
-	ln -sf "${theme_dir}/gtk-4.0/gtk-dark.css" "$HOME/.config/gtk-4.0/gtk-dark.css"
-	flatpak override --user --filesystem=xdg-config/gtk-4.0
 fi
+
+# GTK 4
+rm -rf 					   "$HOME/.config/gtk-4.0/{assets,gtk.css,gtk-dark.css}"
+mkdir -p 				   "$HOME/.config/gtk-4.0"
+ln -sf "${theme_dir}/gtk-4.0/assets"       "$HOME/.config/gtk-4.0/"
+ln -sf "${theme_dir}/gtk-4.0/gtk.css"      "$HOME/.config/gtk-4.0/gtk.css"
+ln -sf "${theme_dir}/gtk-4.0/gtk-dark.css" "$HOME/.config/gtk-4.0/gtk-dark.css"
+flatpak override --user --filesystem=xdg-config/gtk-4.0
 
 # Cursor
 mkdir -p $HOME/.icons/default && echo -e "[Icon Theme]\nInherits=${cursor_theme}" > $HOME/.icons/default/index.theme
