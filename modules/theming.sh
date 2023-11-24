ad48cb89433251687b1563605bade7609d6c4f79
xelser <dkzenzuri@gmail.com>
2023-11-25 00:13:57 +0800

    auto-update @ debian

--- modules/theming.sh
+++ modules/theming.sh
@@ -5,25 +5,25 @@ if [[ ${wm_de} == "xfce" ]]; then
 	gtk_theme="$(xfconf-query -c xsettings -p /Net/ThemeName -v)"
 	icon_theme="$(xfconf-query -c xsettings -p /Net/IconThemeName -v)"
 	cursor_theme="$(xfconf-query -c xsettings -p /Gtk/CursorThemeName -v)"
-	sans_font="$(xfconf-query -c xsettings -p /Gtk/FontName -v)"
-	mono_font="$(xfconf-query -c xsettings -p /Gtk/MonospaceFontName -v)"
+	#sans_font="$(xfconf-query -c xsettings -p /Gtk/FontName -v)"
+	#mono_font="$(xfconf-query -c xsettings -p /Gtk/MonospaceFontName -v)"
 elif [[ ${wm_de} == "cinnamon" ]]; then
 	gtk_theme="$(gsettings get org.cinnamon.desktop.interface gtk-theme | cut -d"'" -f2)"
 	icon_theme="$(gsettings get org.cinnamon.desktop.interface icon-theme | cut -d"'" -f2)"
 	cursor_theme="$(gsettings get org.cinnamon.desktop.interface cursor-theme | cut -d"'" -f2)"
-	sans_font="$(gsettings get org.cinnamon.desktop.interface font-name | cut -d"'" -f2)"
-	mono_font="$(gsettings get org.gnome.desktop.interface monospace-font-name | cut -d"'" -f2)"
+	#sans_font="$(gsettings get org.cinnamon.desktop.interface font-name | cut -d"'" -f2)"
+	#mono_font="$(gsettings get org.gnome.desktop.interface monospace-font-name | cut -d"'" -f2)"
 elif [[ ${wm_de} == "gnome" ]]; then
 	gtk_theme="$(gsettings get org.gnome.desktop.interface gtk-theme | cut -d"'" -f2)"
 	icon_theme="$(gsettings get org.gnome.desktop.interface icon-theme | cut -d"'" -f2)"
 	cursor_theme="$(gsettings get org.gnome.desktop.interface cursor-theme | cut -d"'" -f2)"
-	sans_font="$(gsettings get org.gnome.desktop.interface font-name | cut -d"'" -f2)"
-	mono_font="$(gsettings get org.gnome.desktop.interface monospace-font-name | cut -d"'" -f2)"
+	#sans_font="$(gsettings get org.gnome.desktop.interface font-name | cut -d"'" -f2)"
+	#mono_font="$(gsettings get org.gnome.desktop.interface monospace-font-name | cut -d"'" -f2)"
 elif [[ -f $HOME/.config/gtk-3.0/settings.ini ]]; then
 	gtk_theme="$(cat $HOME/.config/gtk-3.0/settings.ini | grep 'gtk-theme-name' | cut -d'=' -f2)"
 	icon_theme="$(cat $HOME/.config/gtk-3.0/settings.ini | grep 'gtk-icon-theme-name' | cut -d'=' -f2)"
 	cursor_theme="$(cat $HOME/.config/gtk-3.0/settings.ini | grep 'gtk-cursor-theme-name' | cut -d'=' -f2)"
-	sans_font="$(cat $HOME/.config/gtk-3.0/settings.ini | grep 'gtk-font-name' | cut -d'=' -f2)"
+	#sans_font="$(cat $HOME/.config/gtk-3.0/settings.ini | grep 'gtk-font-name' | cut -d'=' -f2)"
 fi
 
 if [ -d /usr/share/themes/${gtk_theme} ]; then
@@ -46,7 +46,7 @@ if [[ ! ${wm_de} == "gnome" ]]; then
 	fi
 
 	flatpak override --user --filesystem=xdg-data/themes:ro
-	flatpak override --user --env=GTK_THEME=${gtk_theme}
+	#flatpak override --user --env=GTK_THEME=${gtk_theme}
 
 	# GTK 4
 	rm -rf 					   "$HOME/.config/gtk-4.0/{assets,gtk.css,gtk-dark.css}"
