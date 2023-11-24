fde5d89ad6b30b30792cf18b878ebfdbab0857f0
xelser <dkzenzuri@gmail.com>
2023-11-24 23:58:09 +0800

    auto-update @ debian

--- modules/stylepak.sh
+++ modules/stylepak.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Install Dependencies
-if [ -f /usr/bin/pacman ]; then sudo pacman -S ostree appstream-glib glib2 --needed --noconfirm
-elif [ -f /usr/bin/zypper ]; then sudo zypper install libostree appstream-glib glib2 -y
-elif [ -f /usr/bin/apt ]; then sudo apt install ostree appstream-util libglib2.0-dev --yes
-elif [ -f /usr/bin/dnf ]; then sudo dnf install ostree libappstream-glib glib2 --assumeyes
-fi
+# DEPENDENCIES
+[ -f /usr/bin/pacman ] && sudo pacman -S --needed --noconfirm ostree appstream-glib #glib2
+[ -f /usr/bin/nala ] && sudo nala install --assume-yes ostree appstream-util #libglib2.0-dev
+[ -f /usr/bin/dnf ] && sudo dnf install --assumeyes ostree libappstream-glib #glib2
 
-# Install Stylepak
-[ ! -f /usr/bin/stylepak ] && cd /tmp/ && git clone https://github.com/refi64/stylepak.git && sudo mv /tmp/stylepak/stylepak /usr/bin/
+# DOWNLOAD
+[ ! -f /usr/bin/stylepak ] && cd /tmp/ && git clone https://github.com/refi64/stylepak && sudo mv /tmp/stylepak/stylepak /usr/bin/
