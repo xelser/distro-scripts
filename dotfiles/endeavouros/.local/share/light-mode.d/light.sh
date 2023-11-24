bc35e05979129a474cff040029e423c56674fb15
xelser <dkzenzuri@gmail.com>
2023-11-25 00:16:18 +0800

    auto-update @ endeavour

--- dotfiles/endeavouros/.local/share/light-mode.d/light.sh
+++ dotfiles/endeavouros/.local/share/light-mode.d/light.sh
@@ -11,3 +11,9 @@ gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
 gsettings set org.gnome.desktop.interface gtk-theme 'Matcha-light-azul'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Light'
 gsettings set org.gnome.desktop.interface cursor-theme 'Qogir-cursors'
+
+# flatpak
+flatpak override --user --env=GTK_THEME=Matcha-light-azul
+
+# update system
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/modules/theming.sh)"
