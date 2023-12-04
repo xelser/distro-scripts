24e611679ea49afe8bfa9e2ba095e7d654e359d6
xelser <dkzenzuri@gmail.com>
2023-12-04 19:57:22 +0800

    auto-update @ endeavour

--- dotfiles/endeavouros/.local/share/dark-mode.d/dark.sh
+++ dotfiles/endeavouros/.local/share/dark-mode.d/dark.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # xsettings
-xfconf-query -cn xfwm4 -pn /general/theme -t string -s "Matcha-dark-azul"
-xfconf-query -cn xsettings -pn /Net/ThemeName -t string -s "Matcha-dark-azul"
+xfconf-query -cn xfwm4 -pn /general/theme -t string -s "Edge-dark-purple"
+xfconf-query -cn xsettings -pn /Net/ThemeName -t string -s "Edge-dark-purple"
 xfconf-query -cn xsettings -pn /Net/IconThemeName -t string -s "Papirus-Dark"
 xfconf-query -cn xsettings -pn /Gtk/CursorThemeName -t string -s "Qogir-white-cursors"
 
 # gsettings
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-gsettings set org.gnome.desktop.interface gtk-theme 'Matcha-dark-azul'
+gsettings set org.gnome.desktop.interface gtk-theme 'Edge-dark-purple'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 gsettings set org.gnome.desktop.interface cursor-theme 'Qogir-white-cursors'
 
@@ -19,7 +19,7 @@ xfconf-query -cn xfce4-terminal -pn /tab-activity-color -t string -s "#55393D"
 xfconf-query -cn xfce4-terminal -pn /color-palette -t string -s "#202023;#EC7279;#DEB974;#A0C980;#6CB6EB;#D38AEA;#5DBBC1;#C5CDD9;#202023;#EC7279;#DEB974;#A0C980;#6CB6EB;#D38AEA;#5DBBC1;#C5CDD9"
 
 # flatpak
-flatpak override --user --env=GTK_THEME=Matcha-dark-azul
+flatpak override --user --env=GTK_THEME=Edge-dark-purple
 
 # update system
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/modules/theming.sh)"
