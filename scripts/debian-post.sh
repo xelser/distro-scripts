33b2524287da0bdf3b523d852ed42494c524da9e
xelser <dkzenzuri@gmail.com>
2024-01-09 15:25:42 +0800

    auto-update @ debian

--- scripts/debian-post.sh
+++ scripts/debian-post.sh
@@ -10,18 +10,22 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/ma
 [ -f /usr/bin/nvim ] && sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
         --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
        
-[ -f /usr/bin/gedit ] && dconf write /org/gnome/gedit/preferences/editor/editor-font "'UbuntuMono Nerd Font 12'"
+if [ -f /usr/bin/gedit ]; then
+        dconf write /org/gnome/gedit/preferences/editor/scheme "'gruvbox-material-hard-dark'"
+        dconf write /org/gnome/gedit/preferences/editor/editor-font "'UbuntuMono Nerd Font 12'"
+        dconf write /org/gnome/gedit/preferences/editor/use-default-font "false"
+fi
 
 # rofi (launcher and powermenu)
 cd /tmp/ && git clone --depth=1 https://github.com/xelser/rofi.git && cd rofi && chmod +x setup.sh && ./setup.sh && cd
 
 sed -i 's/style-1/style-3/g' $HOME/.config/rofi/launchers/type-4/launcher.sh
 sed -i 's/onedark/gruvbox-material-hard-dark/g' $HOME/.config/rofi/launchers/type-4/shared/colors.rasi
-sed -i 's/Iosevka Nerd Font 10/UbuntuMono Nerd Font 12/g' $HOME/.config/rofi/launchers/type-4/shared/fonts.rasi
+sed -i 's/Iosevka Nerd Font 10/UbuntuMono Nerd Font 11/g' $HOME/.config/rofi/launchers/type-4/shared/fonts.rasi
 
 sed -i 's/style-1/style-5/g' $HOME/.config/rofi/powermenu/type-1/powermenu.sh
 sed -i 's/onedark/gruvbox-material-hard-dark/g' $HOME/.config/rofi/powermenu/type-1/shared/colors.rasi
-sed -i 's/JetBrains Mono Nerd Font 10/UbuntuMono Nerd Font 12/g' $HOME/.config/rofi/powermenu/type-1/shared/fonts.rasi
+sed -i 's/JetBrains Mono Nerd Font 10/UbuntuMono Nerd Font 11/g' $HOME/.config/rofi/powermenu/type-1/shared/fonts.rasi
 
 # dunst
 sed -i 's/origin = top-right/origin = bottom-right/g' $HOME/.config/dunst/dunstrc
@@ -38,3 +42,5 @@ systemctl enable --user mpd
 #systemctl enable --user darkman
 systemctl enable --user syncthing
 
+# debloat
+sudo nala purge --assume-yes zutty
