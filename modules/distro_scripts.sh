a973733e9bf14125bce3883ed6513c8125448775
xelser <dkzenzuri@gmail.com>
2024-01-09 15:15:35 +0800

    auto-update @ debian

--- modules/distro_scripts.sh
+++ modules/distro_scripts.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-exec_editor="$(alias edit | cut -d"'" -f2)"
-[ -f /usr/bin/firefox ] && exec_firefox="firefox" || exec_firefox="flatpak run org.mozilla.firefox"
+[ -f /usr/bin/firefox ] && export exec_firefox="firefox" \
+	|| export exec_firefox="flatpak run org.mozilla.firefox"
 
+# Remove old Keys
+rm -rf $HOME/.ssh/
+
+# Profile for git
 git config --global user.email "dkzenzuri@gmail.com"
 git config --global user.name "$USER"
 git config --global pull.rebase false
@@ -13,18 +17,18 @@ if [ ! -f $HOME/.ssh/id_ed25519.pub ]; then
 	eval "$(ssh-agent -s)" >&/dev/null && ssh-add $HOME/.ssh/id_ed25519 >&/dev/null
 	echo -e "${distro_id}@${machine}\n" > $HOME/tmp
 	cat $HOME/.ssh/id_ed25519.pub >> $HOME/tmp
-	${exec_editor} $HOME/tmp & disown
-	${exec_firefox} https://github.com/settings/keys > /tmp/distro_scripts.pid
+	${exec_firefox} https://github.com/settings/keys $HOME/tmp > /tmp/distro_scripts.pid
 fi
 
 # Update Local Repo
 if [ -d $HOME/Documents/distro-scripts ]; then
 	cd $HOME/Documents/distro-scripts && git fetch && git pull && git add .
 	git commit -m "auto-update @ $(hostname)" && git push
-elif [ ! -f /tmp/distro_scripts.pid ]; then
+else
 	echo -e "StrictHostKeyChecking no\n" > $HOME/.ssh/config
 	cd $HOME/Documents && git clone git@github.com:xelser/distro-scripts.git
 fi
 
-# Delete tmp
-rm $HOME/tmp
+# Delete files
+rm $HOME/tmp $HOME/distro_scripts.sh
+
