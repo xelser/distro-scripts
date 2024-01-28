fcc82b7b16eb5d4b276dd815a8d482263b223a98
xelser <dkzenzuri@gmail.com>
2024-01-28 19:01:01 +0800

    auto-update @ fedora

--- themes/pack-edge.sh
+++ themes/pack-edge.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # backgrounds
-cd /tmp/ && git clone https://github.com/xelser/catppuccin-backgrounds
-sudo cp -rf catppuccin-backgrounds/backgrounds /usr/share/
+#cd /tmp/ && git clone --depth 1 https://github.com/xelser/catppuccin-backgrounds
+#sudo cp -rf catppuccin-backgrounds/backgrounds /usr/share/
 
 # gtk
-cd /tmp/ && git clone https://github.com/xelser/edge-gtk
+cd /tmp/ && git clone --depth 1 https://github.com/xelser/edge-gtk
 cd edge-gtk && sudo ./install.sh
 
 # cursors
@@ -19,7 +19,7 @@ cd edge-gtk && sudo ./install.sh
 [[ ${distro_id} == "linuxmint" ]] 	&& color="green"
 [[ ${distro_id} == "endeavouros" ]] && color="purple"
 wget -qO- https://git.io/papirus-icon-theme-install | sh
-cd /tmp/ && git clone https://github.com/xelser/edge-papirus-folders
+cd /tmp/ && git clone --depth 1 https://github.com/xelser/edge-papirus-folders
 cd edge-papirus-folders && sudo cp -rf src/* /usr/share/icons/Papirus/
 papirus_folders=(Papirus Papirus-Dark Papirus-Light ePapirus ePapirus-Dark)
 for icon_theme in "${papirus_folders[@]}"; do 
@@ -28,13 +28,13 @@ done
 
 # gtksourceview
 mkdir -p $HOME/.local/share/gtksourceview-{3.0,4}/styles
-cd /tmp/ && git clone https://github.com/xelser/edge-gtksourceview
+cd /tmp/ && git clone --depth 1 https://github.com/xelser/edge-gtksourceview
 cp -rf /tmp/edge-gtksourceview/*.xml $HOME/.local/share/gtksourceview-3.0/styles/
 ln -sf $HOME/.local/share/gtksourceview-3.0/styles/edge*.xml $HOME/.local/share/gtksourceview-4/styles/
 
 # terminals
 if [ -f /usr/bin/xfce4-terminal ]; then
-	cd /tmp/ && git clone https://github.com/xelser/edge-xfce4-terminal && mkdir -p $HOME/.local/share/xfce4/terminal/colorschemes
+	cd /tmp/ && git clone --depth 1 https://github.com/xelser/edge-xfce4-terminal && mkdir -p $HOME/.local/share/xfce4/terminal/colorschemes
 	cp -rf /tmp/edge-xfce4-terminal/*.theme $HOME/.local/share/xfce4/terminal/colorschemes/
 fi
 
