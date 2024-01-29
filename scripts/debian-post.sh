34837cd8bae769cbf1fb3b000ebc9fe4ac1ae381
xelser <dkzenzuri@gmail.com>
2024-01-29 11:33:21 +0800

    auto-update @ endeavouros

--- scripts/debian-post.sh
+++ scripts/debian-post.sh
@@ -3,10 +3,10 @@
 # themes
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/themes/pack-gruvbox.sh)"
 
-# set fonts
+# fonts
 dconf write /org/gnome/desktop/interface/font-name "'Ubuntu 10'"
-dconf write /org/gnome/desktop/interface/monospace-font-name "'UbuntuMono Nerd Font 12'"
-sed -i 's/font = Monospace 8/font = UbuntuMono Nerd Font 12/g' $HOME/.config/dunst/dunstrc
+dconf write /org/gnome/desktop/interface/monospace-font-name "'Ubuntu Mono 12'"
+sed -i 's/font = Monospace 8/font = Ubuntu Mono 11/g' $HOME/.config/dunst/dunstrc
 
 # vim/neovim plug (text editors)
 [ -f /usr/bin/vim ] && curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -22,15 +22,15 @@ if [ -f /usr/bin/gedit ]; then
 fi
 
 # rofi (launcher and powermenu)
-cd /tmp/ && git clone --depth=1 https://github.com/xelser/rofi.git && cd rofi && chmod +x setup.sh && ./setup.sh && cd
+cd /tmp/ && git clone --depth 1 https://github.com/xelser/rofi.git && cd rofi && chmod +x setup.sh && ./setup.sh && cd
 
 sed -i 's/style-1/style-3/g' $HOME/.config/rofi/launchers/type-4/launcher.sh
 sed -i 's/onedark/gruvbox-material-hard-dark/g' $HOME/.config/rofi/launchers/type-4/shared/colors.rasi
-sed -i 's/Iosevka Nerd Font 10/UbuntuMono Nerd Font 11/g' $HOME/.config/rofi/launchers/type-4/shared/fonts.rasi
+sed -i 's/Iosevka Nerd Font 10/Ubuntu Mono 11/g' $HOME/.config/rofi/launchers/type-4/shared/fonts.rasi
 
 sed -i 's/style-1/style-5/g' $HOME/.config/rofi/powermenu/type-1/powermenu.sh
 sed -i 's/onedark/gruvbox-material-hard-dark/g' $HOME/.config/rofi/powermenu/type-1/shared/colors.rasi
-sed -i 's/JetBrains Mono Nerd Font 10/UbuntuMono Nerd Font 11/g' $HOME/.config/rofi/powermenu/type-1/shared/fonts.rasi
+sed -i 's/JetBrains Mono Nerd Font 10/Ubuntu Mono 11/g' $HOME/.config/rofi/powermenu/type-1/shared/fonts.rasi
 
 # flameshot directory
 mkdir -p $HOME/Pictures/Screenshots
