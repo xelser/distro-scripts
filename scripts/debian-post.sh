730487ffacb7a891bd2f5f85e7bf351c73d5ac15
xelser <dkzenzuri@gmail.com>
2024-02-01 00:17:07 +0800

    auto-update @ debian

--- scripts/debian-post.sh
+++ scripts/debian-post.sh
@@ -26,11 +26,11 @@ cd /tmp/ && git clone --depth 1 https://github.com/xelser/rofi.git && cd rofi &&
 
 sed -i 's/style-1/style-3/g' $HOME/.config/rofi/launchers/type-4/launcher.sh
 sed -i 's/onedark/gruvbox-material-hard-dark/g' $HOME/.config/rofi/launchers/type-4/shared/colors.rasi
-sed -i 's/Iosevka Nerd Font 10/Ubuntu Mono 12/g' $HOME/.config/rofi/launchers/type-4/shared/fonts.rasi
+sed -i 's/Iosevka Nerd Font 10/UbuntuMono Nerd Font 11/g' $HOME/.config/rofi/launchers/type-4/shared/fonts.rasi
 
 sed -i 's/style-1/style-5/g' $HOME/.config/rofi/powermenu/type-1/powermenu.sh
 sed -i 's/onedark/gruvbox-material-hard-dark/g' $HOME/.config/rofi/powermenu/type-1/shared/colors.rasi
-sed -i 's/JetBrains Mono Nerd Font 10/Ubuntu Mono 12/g' $HOME/.config/rofi/powermenu/type-1/shared/fonts.rasi
+sed -i 's/JetBrains Mono Nerd Font 10/UbuntuMono Nerd Font 11/g' $HOME/.config/rofi/powermenu/type-1/shared/fonts.rasi
 
 # flameshot directory
 mkdir -p $HOME/Pictures/Screenshots
