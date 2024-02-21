9c38e52133bda310fb0d07f04dc773e90b85fc5f
xelser <dkzenzuri@gmail.com>
2024-02-21 12:54:58 +0800

    auto-update @ debian

--- scripts/debian-post.sh
+++ scripts/debian-post.sh
@@ -24,24 +24,23 @@ if [ -f /usr/bin/gedit ]; then
 fi
 
 # rofi (launcher and powermenu)
-cd /tmp/ && git clone --depth 1 https://github.com/xelser/rofi.git && cd rofi && chmod +x setup.sh && ./setup.sh && cd
+if [ ${wm_de} = "i3" ]; then
+        cd /tmp/ && git clone --depth 1 https://github.com/xelser/rofi.git && cd rofi && chmod +x setup.sh && ./setup.sh && cd
 
-sed -i 's/style-1/style-3/g' $HOME/.config/rofi/launchers/type-4/launcher.sh
-sed -i 's/onedark/gruvbox-material-hard-dark/g' $HOME/.config/rofi/launchers/type-4/shared/colors.rasi
-sed -i 's/Iosevka Nerd Font 10/UbuntuMono Nerd Font 11/g' $HOME/.config/rofi/launchers/type-4/shared/fonts.rasi
-
-sed -i 's/style-1/style-5/g' $HOME/.config/rofi/powermenu/type-1/powermenu.sh
-sed -i 's/onedark/gruvbox-material-hard-dark/g' $HOME/.config/rofi/powermenu/type-1/shared/colors.rasi
-sed -i 's/JetBrains Mono Nerd Font 10/UbuntuMono Nerd Font 11/g' $HOME/.config/rofi/powermenu/type-1/shared/fonts.rasi
+        sed -i 's/style-1/style-3/g' $HOME/.config/rofi/launchers/type-4/launcher.sh
+        sed -i 's/onedark/gruvbox-material-hard-dark/g' $HOME/.config/rofi/launchers/type-4/shared/colors.rasi
+        sed -i 's/Iosevka Nerd Font 10/UbuntuMono Nerd Font 11/g' $HOME/.config/rofi/launchers/type-4/shared/fonts.rasi
+        
+        sed -i 's/style-1/style-5/g' $HOME/.config/rofi/powermenu/type-1/powermenu.sh
+        sed -i 's/onedark/gruvbox-material-hard-dark/g' $HOME/.config/rofi/powermenu/type-1/shared/colors.rasi
+        sed -i 's/JetBrains Mono Nerd Font 10/UbuntuMono Nerd Font 11/g' $HOME/.config/rofi/powermenu/type-1/shared/fonts.rasi
+fi
 
 # flameshot directory
 mkdir -p $HOME/Pictures/Screenshots
 
-# user systemd daemons
-systemctl enable --user mpd
-
 # debloat
-sudo nala purge --assume-yes zutty
+sudo nala purge --assume-yes zutty xterm
 
 # numlock
 #dconf write /org/gnome/desktop/peripherals/keyboard/numlock-state "true"
