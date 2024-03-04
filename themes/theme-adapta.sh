3b5dbb91efeea3933e2a8b0c9086aa381f83c057
xelser <dkzenzuri@gmail.com>
2024-03-04 16:00:22 +0800

    auto-update @ debian

--- themes/theme-adapta.sh
+++ themes/theme-adapta.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # Dependencies
-[ -f /usr/bin/pacman ] && sudo pacman -S --needed --noconfirm parallel
 [ -f /usr/bin/nala ] && sudo nala install --assume-yes parallel sassc inkscape automake autoconf \
 	pkg-config libxml2 libxml2-utils libglib2.0-dev libgdk-pixbuf2.0-dev
 [ -f /usr/bin/dnf ] && sudo dnf install --assumeyes parallel
 
 # Delete old files
-rm -rf $HOME/.themes/Adapta* /tmp/adapta-gtk-theme
+sudo rm -rf /usr/share/themes/Adapta* $HOME/.themes/Adapta* /tmp/adapta-gtk-theme
 
 # Install
 cd /tmp/ && git clone https://github.com/adapta-project/adapta-gtk-theme && cd adapta-gtk-theme
-./autogen.sh --prefix=$HOME/.local --enable-parallel --enable-gtk_next --enable-plank 
-make && make install && mv $HOME/.local/share/themes/Adapta* $HOME/.themes/
+./autogen.sh --prefix=/usr --enable-parallel --enable-gtk_next --enable-plank 
+make && sudo make install
