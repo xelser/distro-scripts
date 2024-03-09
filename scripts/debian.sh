71f3d8b1e0590dec42cc0ec09251038e6edea7f8
xelser <dkzenzuri@gmail.com>
2024-03-09 17:14:58 +0800

    auto-update @ debian

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -38,7 +38,7 @@ nala install --assume-yes autoconf gcc make pkg-config libpam0g-dev \
   libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev \
   libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev \
   libxcb-image0-dev libxcb-util0-dev libxcb-xrm-dev libxkbcommon-dev \
-  libxkbcommon-x11-dev libjpeg-dev
+  libxkbcommon-x11-dev libjpeg-dev imagemagick
 cd /tmp/ && git clone https://github.com/Raymo111/i3lock-color
 cd i3lock-color && ./install-i3lock-color.sh && \
   wget https://raw.githubusercontent.com/betterlockscreen/betterlockscreen/main/install.sh -O - -q | sudo bash -s system
