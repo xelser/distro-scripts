061b8bc577b3c08e292bc6b101ca800cc4fd34c7
xelser <dkzenzuri@gmail.com>
2024-03-11 01:04:14 +0800

    auto-update @ debian

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -15,7 +15,7 @@ nala install --assume-yes xorg lightdm{,-gtk-greeter-settings} numlockx \
   thunar-{archive-plugin,volman} gvfs-{backends,fuse}
 
 # INSTALL: Debian i3
-nala install --assume-yes i3-wm picom feh polybar alacritty neovim \
+nala install --assume-yes i3-wm picom polybar alacritty neovim \
   imv mpv rofi dunst libnotify-bin mate-polkit lxappearance \
   xarchiver gedit flameshot
 
@@ -38,7 +38,7 @@ nala install --assume-yes autoconf gcc make pkg-config libpam0g-dev \
   libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev \
   libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev \
   libxcb-image0-dev libxcb-util0-dev libxcb-xrm-dev libxkbcommon-dev \
-  libxkbcommon-x11-dev libjpeg-dev imagemagick
+  libxkbcommon-x11-dev libjpeg-dev imagemagick feh
 cd /tmp/ && git clone https://github.com/Raymo111/i3lock-color
 cd i3lock-color && ./install-i3lock-color.sh && \
   wget https://raw.githubusercontent.com/betterlockscreen/betterlockscreen/main/install.sh -O - -q | sudo bash -s system
@@ -67,9 +67,6 @@ autologin-session=i3
 greeter-hide-users=false
 " >> /etc/lightdm/lightdm.conf
 systemctl enable lightdm
-
-# betterlockscreen
-betterlockscreen -u "/run/media/xelser/Media/Pictures/Gruvbox" --fx dim 50
 
 ################################### THEMES ###################################
 
