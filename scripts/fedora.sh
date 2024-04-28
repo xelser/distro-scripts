18c9347acdf97fe45d8ac39405329e82450d71bb
xelser <dkzenzuri@gmail.com>
2024-04-28 11:58:43 +0800

    auto-update @ fedora

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -74,6 +74,6 @@ AutomaticLoginEnable=True" /etc/gdm/custom.conf
 if [ ! -f /.flag ]; then
 	${source_dir}/themes/pack-libadwaita.sh
 	${source_dir}/themes/icon-tela-circle.sh
-	${source_dir}/themes/cursor-bibata.sh
+	${source_dir}/themes/cursor-bibata.sh v2.0.4
 	${source_dir}/themes/fonts-nerd.sh RobotoMono
 fi
