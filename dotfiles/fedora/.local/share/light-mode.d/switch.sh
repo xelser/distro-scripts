5b1d110212a49618fdf9c9fca08254fe266c0d88
xelser <dkzenzuri@gmail.com>
2024-04-28 13:17:29 +0800

    auto-update @ fedora

--- dotfiles/fedora/.local/share/light-mode.d/switch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p $HOME/.config/Kvantum/
-mkdir -p $HOME/.config/qt5ct/
-
-cp -rf $HOME/.local/share/light-mode.d/kvantum.kvconfig	$HOME/.config/Kvantum/
-cp -rf $HOME/.local/share/light-mode.d/qt5ct.conf	$HOME/.config/qt5ct/
-
