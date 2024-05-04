bf21cdaeceecd249703a59fc7c25a18a2f4b75a7
xelser <dkzenzuri@gmail.com>
2024-05-04 12:40:57 +0800

    auto-update @ fedora

---
+++ modules/grub2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+### Grub2 Themes
+### See https://github.com/vinceliuice/grub2-themes
+
+# Clone
+cd /tmp/ && git clone https://github.com/vinceliuice/grub2-themes.git
+
+# Install
+cd grub2-themes && sudo ./install.sh -t vimix -i white
