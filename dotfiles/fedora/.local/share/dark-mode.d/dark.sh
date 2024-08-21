ec423ce4733744faaf62a4c1e0b7e967ed51cbca
xelser <dkzenzuri@gmail.com>
2024-08-21 19:24:31 +0800

    auto-update @ fedora

--- dotfiles/fedora/.local/share/dark-mode.d/dark.sh
+++ dotfiles/fedora/.local/share/dark-mode.d/dark.sh
@@ -7,6 +7,7 @@
 #cp -rf $HOME/.local/share/dark-mode.d/qt5ct.conf	$HOME/.config/qt5ct/
 
 # Main
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Tela-circle-dark'
 gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'
--- dotfiles/fedora/.local/share/light-mode.d/light.sh
+++ dotfiles/fedora/.local/share/light-mode.d/light.sh
@@ -7,6 +7,7 @@
 #cp -rf $HOME/.local/share/light-mode.d/qt5ct.conf	$HOME/.config/qt5ct/
 
 # Main
+gsettings set org.gnome.desktop.interface color-scheme 'default'
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
 gsettings set org.gnome.desktop.interface icon-theme 'Tela-circle'
 gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Classic'
