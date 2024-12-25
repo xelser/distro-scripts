4ee51c8d6fded689e01e9c2dc9c683e21ebb4514
xelser <dkzenzuri@gmail.com>
2024-12-25 11:29:06 +0800

    auto-update @ arch

--- modules/gaming.sh
+++ modules/gaming.sh
@@ -46,9 +46,12 @@ nvidia_prime () {
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/modules/lutris_wine_dep.sh)"
 
 # Install: Steam, Lutris, Gamemode & MangoHud
-[ -f /usr/bin/pacman ] && sudo pacman -S --needed --noconfirm {lib32-,}gamemode {lib32-,}mangohud lutris steam vulkan-tools mesa-demos
-[ -f /usr/bin/dnf5 ] && sudo dnf5 install --assumeyes gamemode.{x86_64,i686} mangohud.{x86_64,i686} lutris steam vulkan-tools mesa-demos
-[ -f /usr/bin/nala ] && sudo nala install --assume-yes gamemode{,:i386} mangohud lutris steam vulkan-tools mesa-utils-bin
+[ -f /usr/bin/pacman ] && sudo pacman -S --needed --noconfirm steam lutris libayatana-appindicator \
+	{lib32-,}gamemode {lib32-,}mangohud vulkan-tools mesa-demos
+[ -f /usr/bin/dnf5 ] && sudo dnf5 install --assumeyes steam lutris libayatana-appindicator-gtk3 \
+	gamemode.{x86_64,i686} mangohud.{x86_64,i686} vulkan-tools mesa-demos
+[ -f /usr/bin/nala ] && sudo nala install --assume-yes steam lutris libayatana-appindicator3 \
+	gamemode{,:i386} mangohud vulkan-tools mesa-utils-bin
 
 ################################### FLATPAK ##################################
 
