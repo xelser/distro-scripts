ac403f8c2a48f8109e79d2939ee8e7ced695aca5
xelser <dkzenzuri@gmail.com>
2024-12-25 16:14:09 +0800

    auto-update @ arch

--- modules/gaming.sh
+++ modules/gaming.sh
@@ -70,8 +70,8 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/ma
 ################################### CONFIG ###################################
 
 # Steam (Background)
-cp -r /usr/share/applications/steam.desktop $HOME/.config/autostart/
-sed -i s"/steam-runtime/steam-runtime -silent/"g $HOME/.config/autostart/steam.desktop
+cp -r /usr/share/applications/steam.desktop $HOME/.local/share/applications/
+sed -i s"/\/usr\/bin\/steam-runtime/mangohud gamemoderun \/usr\/bin\/steam-runtime -silent/"g $HOME/.local/share/applications/steam.desktop
 
 # Gamemode
 sudo usermod -aG gamemode $(whoami)
