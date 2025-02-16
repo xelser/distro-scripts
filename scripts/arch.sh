f63712891bc61bde55917480bef921a557113ff0
xelser <dkzenzuri@gmail.com>
2025-02-16 19:04:55 +0800

    auto-update @ fedora

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -177,21 +177,21 @@ arch_wm () { arch-chroot /mnt /bin/bash << EOF
 
 # Window Manager Packages
 pacman -S --needed --noconfirm xdg-desktop-portal-{wlr,gtk,hyprland} ttf-fira{-sans,code-nerd} \
-	greetd{,-gtkgreet} mate-polkit nwg-look nm-connection-editor flameshot grim wallutils feh swaybg imv mpv \
+	sddm mate-polkit nwg-look nm-connection-editor flameshot grim wallutils feh swaybg imv mpv \
 	brightnessctl gammastep alacritty dunst rofi atril pluma engrampa nemo i3-wm polybar picom
 
-	#openbox obconf tint2 sway waybar hyprland kvantum-qt5 qt5ct
+	#openbox obconf tint2 sway waybar hyprland kvantum-qt5 qt5ct greetd{,-gtkgreet} 
 
 # sddm
-#echo -e "[Autologin]\nUser=${user}\nSession=i3" >> /etc/sddm.conf
-#echo -e "\n[General]\nNumlock=on" >> /etc/sddm.conf
-#systemctl enable sddm
+echo -e "[Autologin]\nUser=${user}\nSession=i3" >> /etc/sddm.conf
+echo -e "\n[General]\nNumlock=on" >> /etc/sddm.conf
+systemctl enable sddm
 
 # greetd
-echo -e "[terminal]\nvt = 1\n\n[greeter]\ntype = 'gtkgreet'\n
-[session]\ncommand = '/usr/bin/i3-with-shmlog'
-user = 'xelser'" > /etc/greetd/greetd.conf
-systemctl enable greetd
+#echo -e "[terminal]\nvt = 1\n\n[greeter]\ntype = 'gtkgreet'\n
+#[session]\ncommand = '/usr/bin/i3-with-shmlog'
+#user = 'xelser'" > /etc/greetd/greetd.conf
+#systemctl enable greetd
 
 EOF
 }
