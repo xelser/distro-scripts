1a66830676f79d6590e843ca7d23f5d1589bc6b9
xelser <dkzenzuri@gmail.com>
2025-03-09 06:50:24 +0800

    auto-update @ fedora

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -30,7 +30,6 @@ sudo dnf copr enable whitehara/htpdate --assumeyes
 sudo dnf config-manager setopt google-chrome.enabled=1
 
 # ADD REPO: Brave Browser
-sudo dnf install dnf-plugins-core --assumeyes 
 sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo --assumeyes 
 
 # ADD REPO: htpdate (COPR)
@@ -41,7 +40,7 @@ sudo dnf install --assumeyes --skip-broken --allowerasing gnome-{builder,console
   file-roller fragments celluloid drawing easyeffects lsp-plugins-lv2 nvim wl-clipboard syncthing libheif-tools \
   htpdate google-chrome-stable brave-browser
 
-  # inkscape telegram discord video-downloader touchegg google-chrome-stable
+  # inkscape telegram discord video-downloader touchegg
   # gnome-shell-extension-{light-style,user-theme} google-roboto-{fonts,mono-fonts,slab-fonts}
 
 # INSTALL: TeamViewer
@@ -86,6 +85,5 @@ if [ ! -f /.flag ]; then
 	${source_dir}/themes/pack-libadwaita.sh
 	${source_dir}/themes/icon-tela-circle.sh
 	${source_dir}/themes/cursor-bibata.sh
-	${source_dir}/themes/firefox-gnome.sh
   ${source_dir}/themes/fonts-adwaita.sh 48
 fi
