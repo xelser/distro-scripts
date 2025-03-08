13532840f6c8554996882daa777ed400c411891a
xelser <dkzenzuri@gmail.com>
2025-03-09 06:40:57 +0800

    auto-update @ fedora

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -18,33 +18,41 @@ sudo dnf remove --assumeyes @guest-desktop-agents @container-management @libreof
   rhythmbox mediawriter simple-scan fedora-bookmarks totem libreoffice-\* gnome-shell-extension-\* \
   gnome-{boxes,contacts,characters,connections,font-viewer,tour,clocks,weather,maps}
 
-# REPO: Google Chrome | RPMFUSION
-#sudo dnf config-manager --set-enabled --assumeyes google-chrome
+# ADD REPO: RPMFUSION
 sudo dnf list --installed | grep -q "rpmfusion" || sudo dnf5 install --assumeyes --skip-broken \
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# UPDATE
+# ADD REPO: htpdate (COPR)
+sudo dnf copr enable whitehara/htpdate --assumeyes
+
+# ADD REPO: Google Chrome
+sudo dnf config-manager setopt google-chrome.enabled=1
+
+# ADD REPO: Brave Browser
+sudo dnf install dnf-plugins-core --assumeyes 
+sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo --assumeyes 
+
+# ADD REPO: htpdate (COPR)
+sudo dnf copr enable whitehara/htpdate --assumeyes
+
+# INSTALL: Fedora Workstation
+sudo dnf install --assumeyes --skip-broken --allowerasing gnome-{builder,console,extensions-app,tweaks} \
+  file-roller fragments celluloid drawing easyeffects lsp-plugins-lv2 nvim wl-clipboard syncthing libheif-tools \
+  htpdate google-chrome-stable brave-browser
+
+  # inkscape telegram discord video-downloader touchegg google-chrome-stable
+  # gnome-shell-extension-{light-style,user-theme} google-roboto-{fonts,mono-fonts,slab-fonts}
+
+# INSTALL: TeamViewer
+sudo dnf install --assumeyes https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm
+
+# FINAL UPDATE
 sudo dnf upgrade @core @sound-and-video @multimedia --exclude=PackageKit-gstreamer-plugin --assumeyes --best --allowerasing --skip-unavailable 
 
 # INSTALL: Fedora Multimedia Codecs (from RPM Fusion https://rpmfusion.org/Howto/Multimedia)
 #sudo dnf swap ffmpeg-free ffmpeg --assumeyes --allowerasing
 #sudo dnf groupupdate sound-and-video multimedia --assumeyes --exclude=PackageKit-gstreamer-plugin
-
-# INSTALL: Fedora Workstation
-sudo dnf install --assumeyes --skip-broken --allowerasing gnome-{builder,console,extensions-app,tweaks} \
-  file-roller fragments celluloid drawing easyeffects lsp-plugins-lv2 nvim wl-clipboard syncthing libheif-tools 
-
-  # inkscape telegram discord video-downloader touchegg google-chrome-stable
-  # gnome-shell-extension-{light-style,user-theme} google-roboto-{fonts,mono-fonts,slab-fonts}
-
-# INSTALL: htpdate
-sudo dnf copr enable whitehara/htpdate --assumeyes
-sudo dnf install htpdate --assumeyes
-sudo systemctl enable htpdate --now
-
-# INSTALL: TeamViewer (rpm)
-sudo dnf install --assumeyes https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm
 
 ################################### CONFIG ###################################
 
@@ -67,6 +75,9 @@ sudo grubby --update-kernel=ALL --args=“plymouth.use-simpledrm”
 append_file "[daemon]
 AutomaticLogin=${user}
 AutomaticLoginEnable=True" /etc/gdm/custom.conf
+
+# HTPDATE
+sudo systemctl enable htpdate --now
 
 ################################### THEMES ###################################
 
