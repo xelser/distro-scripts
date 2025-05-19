97daa6fa3c2bd8840380dd7bdcc2701402a74a82
xelser <dkzenzuri@gmail.com>
2025-05-19 09:24:23 +0800

    auto-update @ fedora

--- themes/firefox-gnome.sh
+++ themes/firefox-gnome.sh
@@ -6,5 +6,5 @@ firefox --headless >&/dev/null & disown && sleep 10 && killall firefox
 # download
 cd /tmp/ && git clone https://github.com/rafaelmardojai/firefox-gnome-theme && cd firefox-gnome-theme 
 
-# install with correct firefox version
-git checkout v$(firefox --version | cut -d ' ' -f 3 | cut -d '.' -f 1) && ./scripts/auto-install.sh
+# install
+./scripts/auto-install.sh
