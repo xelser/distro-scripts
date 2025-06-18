96dec1c3a34f67f254e0fd5e72efe19687f8a165
xelser <dkzenzuri@gmail.com>
2025-06-18 23:33:36 +0800

    auto-update @ fedora

--- scripts/fedora-post.sh
+++ scripts/fedora-post.sh
@@ -12,6 +12,6 @@ gsettings set org.gnome.desktop.interface monospace-font-name "Adwaita Mono 10"
 ################################### FLATPAK ##################################
 
 # INSTALL: Fedora Workstation
-#flatpak install --assumeyes --noninteractive flathub com.google.Chrome
+flatpak install --assumeyes --noninteractive flathub com.google.Chrome io.missioncenter.MissionCenter
   # org.mozilla.firefox com.spotify.Client us.zoom.Zoom org.telegram.desktop com.discordapp.Discord
   # com.rafaelmardojai.Blanket org.gnome.gitlab.YaLTeR.VideoTrimmer org.nickvision.tubeconverter
