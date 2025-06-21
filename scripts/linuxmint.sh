79e5d64c072d241cc125541dd1d236c9a009a18a
xelser <dkzenzuri@gmail.com>
2025-06-22 00:20:51 +0800

    auto-update @ mint

--- scripts/linuxmint.sh
+++ scripts/linuxmint.sh
@@ -7,17 +7,17 @@ sudo sed -i 's/packages.linuxmint.com/mirror.rackspace.com\/linuxmint\/packages/
 #sudo sed -i 's/archive.ubuntu.com/mirror.rise.ph/g' /etc/apt/sources.list.d/official-package-repositories.list
 
 # PACKAGE MANAGER: Nala
-#sudo apt update && sudo apt install nala --yes
+sudo apt update && sudo apt install nala --yes
 
 # DEBLOAT
-sudo apt autoremove --purge --yes rhythmbox hypnotix timeshift papirus-icon-theme libreoffice-*
+sudo nala remove --purge --assume-yes rhythmbox hypnotix timeshift papirus-icon-theme libreoffice-*
 
 # UPDATE
-sudo apt update && sudo apt upgrade --yes
+sudo nala upgrade --assume-yes
 
 # INSTALL: Linux Mint Cinnamon
-sudo apt install --yes build-essential mint-meta-codecs power-profiles-daemon \
-  easyeffects dconf-editor gnome-{builder,disk-utility} gparted nala neovim
+sudo nala install --assume-yes build-essential mint-meta-codecs power-profiles-daemon \
+  syncthing-gtk easyeffects dconf-editor gnome-{builder,disk-utility} gparted neovim
   
   # plank grub-customizer numlockx gpaste gir1.2-gpaste-4.0 openoffice.org-hyphenation 
 
