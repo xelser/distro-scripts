ed7dadf31f317db30c7d3f767d5d8528454fc5b9
xelser <dkzenzuri@gmail.com>
2025-08-18 21:24:43 +0800

    auto-update @ debian

--- dotfiles/debian/.config/autostart/autostart-script.sh
+++ dotfiles/debian/.config/autostart/autostart-script.sh
@@ -4,5 +4,5 @@
 [ -f /usr/bin/cpupower ] && sudo cpupower frequency-set -g performance
 
 # themes
-[ -f $HOME/Documents/distro-scripts/modules/theming.sh ] && $HOME/Documents/distro-scripts/modules/theming.sh 
+[ -f $HOME/Documents/distro-scripts/modules/theming.sh ] && $HOME/Documents/distro-scripts/modules/theming.sh
 
