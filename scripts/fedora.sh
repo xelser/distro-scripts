161e1546ba84fb1daf839b8a65431eca17f8b34d
xelser <dkzenzuri@gmail.com>
2025-09-23 03:11:56 +0800

    auto-update @ fedora

--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -6,43 +6,55 @@ append_file () {
 
 ################################## PACKAGES ##################################
 
-# DISABLE SUSPEND ON AC
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type "nothing"
-
 # PACKAGE MANAGER: DNF
 echo -e "[main]\nkeepcache=True\ndefaultyes=True\ninstall_weak_deps=False\nmax_parallel_downloads=5
 color=always" | sudo tee /etc/dnf/libdnf5.conf.d/20-user-settings.conf 1> /dev/null
-
-# DEBLOAT
-sudo dnf remove --assumeyes @guest-desktop-agents @container-management @libreoffice \
-  rhythmbox mediawriter simple-scan fedora-bookmarks totem ptyxis libreoffice-\* \
-  gnome-{contacts,characters,connections,font-viewer,tour,clocks,weather,maps} \
-  gnome-shell-extension-\*
 
 # ADD REPO: RPMFUSION
 sudo dnf list --installed | grep -q "rpmfusion" || sudo dnf install --assumeyes --skip-broken \
   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+# Fedora Workstation
+if [ $(grep VARIANT_ID /etc/os-release | cut -d '=' -f2) = "workstation" ]; then
+  
+  # DISABLE SUSPEND ON AC
+  gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type "nothing"
+  
+  # DEBLOAT
+  sudo dnf remove --assumeyes @guest-desktop-agents @container-management @libreoffice \
+    rhythmbox mediawriter simple-scan fedora-bookmarks totem ptyxis libreoffice-\* \
+    gnome-{contacts,characters,connections,font-viewer,tour,clocks,weather,maps} \
+    gnome-shell-extension-\*
+
+  # INSTALL: Fedora Workstation
+  sudo dnf install --assumeyes --skip-broken --allowerasing easyeffects lsp-plugins-lv2 \
+    gnome-{builder,console,extensions-app,tweaks} file-roller fragments celluloid drawing \
+    nvim wl-clipboard libheif-tools
+  
+  # inkscape telegram discord video-downloader syncthing touchegg
+  # gnome-shell-extension-{light-style,user-theme} google-roboto-{fonts,mono-fonts,slab-fonts}
+else
+  # INSTALL: Fedora Sway
+  sudo dnf install --assumeyes --skip-broken --allowerasing xdg-desktop-portal-{wlr,gtk} \
+    sway{,bg,idle} greetd seatd waybar wofi mako grimshot imv mpv alacritty brightnessctl \
+    transmission-gtk pavucontrol easyeffects lsp-plugins-lv2 nvim wl-clipboard \
+    mate-polkit atril pluma engrampa caja
+
+  # swayfx nwg-look waypaper autotiling mugshot 
+fi
+
 # UPDATE
 sudo dnf upgrade @core @sound-and-video @multimedia --assumeyes --best --allowerasing \
   --skip-unavailable --exclude=PackageKit-gstreamer-plugin
 
-# INSTALL: Fedora Workstation
-sudo dnf install --assumeyes --skip-broken --allowerasing easyeffects lsp-plugins-lv2 \
-  gnome-{builder,console,extensions-app,tweaks} file-roller fragments celluloid drawing \
-  nvim wl-clipboard libheif-tools
-  
-  # inkscape telegram discord video-downloader syncthing touchegg
-  # gnome-shell-extension-{light-style,user-theme} google-roboto-{fonts,mono-fonts,slab-fonts}
-
-# INSTALL: htpdate (COPR)
-sudo dnf copr enable whitehara/htpdate --assumeyes
-sudo dnf install htpdate --assumeyes
-sudo systemctl enable htpdate --now
-
 # INSTALL: Brave Browser
 curl -fsS https://dl.brave.com/install.sh | sh
+
+# INSTALL: htpdate (COPR)
+#sudo dnf copr enable whitehara/htpdate --assumeyes
+#sudo dnf install htpdate --assumeyes
+#sudo systemctl enable htpdate --now
 
 # INSTALL: Fedora Multimedia Codecs (from RPM Fusion https://rpmfusion.org/Howto/Multimedia)
 #sudo dnf swap ffmpeg-free ffmpeg --assumeyes --allowerasing
@@ -58,12 +70,12 @@ curl -fsS https://dl.brave.com/install.sh | sh
 #sudo grub2-mkconfig -o /etc/grub2-efi.cfg 1> /dev/null
 #sudo grub2-mkconfig -o /boot/grub2/grub.cfg
 
+# Plymouth fix
+#sudo plymouth-set-default-theme -R bgrt
+#sudo grubby --update-kernel=ALL --args=“plymouth.use-simpledrm”
+
 # Set Hostname
 sudo hostnamectl set-hostname --static "fedora"
-
-# Plymouth fix
-sudo plymouth-set-default-theme -R bgrt
-sudo grubby --update-kernel=ALL --args=“plymouth.use-simpledrm”
 
 # GDM
 append_file "[daemon]
