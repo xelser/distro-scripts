6234062647aa9d30310c6290c65d396a5c014af6
xelser <dkzenzuri@gmail.com>
2025-09-23 19:01:05 +0800

    auto-update @ fedora

--- dotfiles/debian/.config/autostart/autostart-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# cpu
-[ -f /usr/bin/cpupower ] && sudo cpupower frequency-set -g performance
-
-# themes
-[ -f $HOME/Documents/distro-scripts/modules/theming.sh ] && $HOME/Documents/distro-scripts/modules/theming.sh
-
--- dotfiles/endeavouros/.config/autostart/autostart-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# keyboard led
-xset led 3
-numlockx
-
-# network time (update upon startup)
-[ -f /bin/htpdate ] && sudo htpdate -D -s -i /run/htpdate.pid www.linux.org 
