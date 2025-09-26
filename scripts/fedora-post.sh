eeeb66ac43abde7eb361bf9458867b5e01fccdeb
xelser <dkzenzuri@gmail.com>
2025-09-26 19:52:14 +0800

    auto-update @ debian

--- scripts/fedora-post.sh
+++ scripts/fedora-post.sh
@@ -1,64 +1,13 @@
 #!/bin/bash
 
-if [ "$(grep VARIANT_ID /etc/os-release | cut -d '=' -f2)" = "workstation" ]; then
-  
-  # themes
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/themes/pack-libadwaita.sh)"
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/themes/icon-tela-circle.sh)"
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/themes/cursor-bibata.sh)"
-  
-  # GNOME Shell Extensions
-  gsettings set org.gnome.shell enabled-extensions []
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/modules/gnome_extensions.sh)"
-  ext_list=($(gnome-extensions list)); for ext in "${ext_list[@]}"; do gnome-extensions enable ${ext}; done
+# GNOME Shell Extensions
+gsettings set org.gnome.shell enabled-extensions []
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/modules/gnome_extensions.sh)"
+ext_list=($(gnome-extensions list)); for ext in "${ext_list[@]}"; do gnome-extensions enable ${ext}; done
 
-  # Set Fonts
-  gsettings set org.gnome.desktop.interface font-name "Adwaita Sans 10"
-  gsettings set org.gnome.desktop.interface monospace-font-name "Adwaita Mono 10"
-
-else
-
-  # themes
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/themes/pack-gruvbox.sh)"
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/themes/cursor-sainnhe-capitaine.sh)"
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/themes/fonts-nerd.sh)" dummy_arg RobotoMono
-
-  # fonts
-  dconf write /org/gnome/desktop/interface/font-name "'Roboto Medium 10'"
-  dconf write /org/gnome/desktop/interface/monospace-font-name "'RobotoMono Nerd Font Medium 9'"
-
-  # notification
-  #if [ -f $HOME/.config/dunst/dunstrc ]; then
-  #  sed -i 's/font = Monospace 8/font = RobotoMono Nerd Font 10/g' $HOME/.config/dunst/dunstrc
-  #  sed -i 's/origin = top-right/origin = bottom-right/g' $HOME/.config/dunst/dunstrc
-  #  sed -i 's/max_icon_size = 128/max_icon_size = 64/g' $HOME/.config/dunst/dunstrc
-  #3  sed -i 's/offset = (10, 50)/offset = (20, 30)/g' $HOME/.config/dunst/dunstrc
-  #fi
-
-  # text editors
-  if [ -f /usr/bin/pluma ]; then
-    gsettings set org.mate.pluma color-scheme 'gruvbox-material-hard-dark'
-    gsettings set org.mate.pluma editor-font 'RobotoMono Nerd Font Medium 9'
-    gsettings set org.mate.pluma highlight-current-line true
-    gsettings set org.mate.pluma display-line-numbers true
-    gsettings set org.mate.pluma toolbar-visible false
-    gsettings set org.mate.pluma tabs-size 2
-  fi
-
-  # rofi (launcher and powermenu)
-  #if [ -f /usr/bin/rofi ]; then
-  #  cd /tmp/ && git clone --depth 1 https://github.com/xelser/rofi.git && cd rofi && chmod +x setup.sh && ./setup.sh && cd
-
-   # sed -i 's/style-1/style-3/g' $HOME/.config/rofi/launchers/type-4/launcher.sh
-   # sed -i 's/onedark/gruvbox-material-hard-dark/g' $HOME/.config/rofi/launchers/type-4/shared/colors.rasi
-   # sed -i 's/Iosevka Nerd Font 10/RobotoMono Nerd Font 10/g' $HOME/.config/rofi/launchers/type-4/shared/fonts.rasi
-
-   # sed -i 's/style-1/style-5/g' $HOME/.config/rofi/powermenu/type-1/powermenu.sh
-   # sed -i 's/onedark/gruvbox-material-hard-dark/g' $HOME/.config/rofi/powermenu/type-1/shared/colors.rasi
-   # sed -i 's/JetBrains Mono Nerd Font 10/RobotoMono Nerd Font 10/g' $HOME/.config/rofi/powermenu/type-1/shared/fonts.rasi
-  #fi
-
-fi
+# Set Fonts
+gsettings set org.gnome.desktop.interface font-name "Adwaita Sans 10"
+gsettings set org.gnome.desktop.interface monospace-font-name "Adwaita Mono 10"
 
 ################################### FLATPAK ##################################
 
--- scripts/fedora.sh
+++ scripts/fedora.sh
@@ -6,15 +6,18 @@ append_file () {
 
 ################################## PACKAGES ##################################
 
+# DISABLE SUSPEND ON AC
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type "nothing"
+
 # PACKAGE MANAGER: DNF
 echo -e "[main]\nkeepcache=True\ndefaultyes=True\ninstall_weak_deps=False\nmax_parallel_downloads=5
 color=always" | sudo tee /etc/dnf/libdnf5.conf.d/20-user-settings.conf 1> /dev/null
 
 # DEBLOAT
-sudo dnf remove --assumeyes @guest-desktop-agents @container-management @libreoffice @admin-tools \
-  gnome-{contacts,characters,connections,font-viewer,tour,clocks,weather,maps} rhythmbox mediawriter \
-  simple-scan fedora-bookmarks totem ptyxis firefox gnome-shell-extension-\* libreoffice-\* \
-  firewalld Thunar rofi-wayland dunst
+sudo dnf remove --assumeyes @guest-desktop-agents @container-management @libreoffice \
+  rhythmbox mediawriter simple-scan fedora-bookmarks totem ptyxis libreoffice-\* \
+  gnome-{contacts,characters,connections,font-viewer,tour,clocks,weather,maps} \
+  gnome-shell-extension-\*
 
 # ADD REPO: RPMFUSION
 sudo dnf list --installed | grep -q "rpmfusion" || sudo dnf install --assumeyes --skip-broken \
@@ -22,51 +25,23 @@ sudo dnf list --installed | grep -q "rpmfusion" || sudo dnf install --assumeyes 
   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # UPDATE
-sudo dnf upgrade @core @sound-and-video @multimedia --assumeyes --best --allowerasing \
-  --skip-unavailable --exclude=PackageKit-gstreamer-plugin
+sudo dnf upgrade @core @sound-and-video @multimedia --exclude=PackageKit-gstreamer-plugin --assumeyes --best --allowerasing --skip-unavailable
 
-# INSTALL: Fedora Base
-sudo dnf install --assumeyes --allowerasing xdg-user-dirs pipewire-pulse wireplumber \
-  easyeffects lsp-plugins-lv2 bluez power-profiles-daemon nvim wl-clipboard 
-
-# INSTALL: Fedora Variants
-if [ "$(grep VARIANT_ID /etc/os-release | cut -d '=' -f2)" = "workstation" ]; then
-  
-  # DISABLE SUSPEND ON AC
-  gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type "nothing"
-
-  # INSTALL: Fedora Workstation
-  sudo dnf install --assumeyes --allowerasing file-roller fragments celluloid \
-    gnome-{builder,console,extensions-app,tweaks} libheif-tools
+# INSTALL: Fedora Workstation
+sudo dnf install --assumeyes --allowerasing file-roller fragments celluloid \
+  gnome-{builder,console,extensions-app,tweaks} easyeffects lsp-plugins-lv2 \
+  nvim wl-clipboard libheif-tools
   
   # inkscape telegram discord video-downloader syncthing touchegg
-  # gnome-shell-extension-{light-style,user-theme}
-else
-
-  # ADD REPO: COPR
-  sudo dnf copr enable solopasha/hyprland --assumeyes
-  sudo dnf copr enable swayfx/swayfx --assumeyes
-  
-  # INSTALL: Fedora Sway
-  sudo dnf install --assumeyes --allowerasing google-roboto-{fonts,mono-fonts,slab-fonts} \
-    sway{fx,bg,idle} wofi wlogout mako brightnessctl grimshot pavucontrol blueman imv mpv \
-    sddm-wayland-sway mate-polkit atril pluma engrampa caja transmission-gtk \
-    waypaper nwg-look xdg-desktop-portal-gtk
-    
-    # autotiling mugshot seatd
-  
-  # CONFIG: sddm
-  sudo systemctl enable sddm
-
-fi
-
-# INSTALL: Brave Browser
-curl -fsS https://dl.brave.com/install.sh | sh
+  # gnome-shell-extension-{light-style,user-theme} google-roboto-{fonts,mono-fonts,slab-fonts}
 
 # INSTALL: htpdate (COPR)
 sudo dnf copr enable whitehara/htpdate --assumeyes
 sudo dnf install htpdate --assumeyes
 sudo systemctl enable htpdate --now
+
+# INSTALL: Brave Browser
+curl -fsS https://dl.brave.com/install.sh | sh
 
 # INSTALL: Fedora Multimedia Codecs (from RPM Fusion https://rpmfusion.org/Howto/Multimedia)
 #sudo dnf swap ffmpeg-free ffmpeg --assumeyes --allowerasing
@@ -77,22 +52,28 @@ sudo systemctl enable htpdate --now
 # Grub
 #sudo sed -i 's/quiet/quiet splash/g' /etc/default/grub
 #sudo sed -i 's/splash splash/splash/g' /etc/default/grub
-#sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=10/g' /etc/default/grub
+#sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=20/g' /etc/default/grub
 #sudo grub2-mkconfig -o /etc/grub2.cfg 1> /dev/null
 #sudo grub2-mkconfig -o /etc/grub2-efi.cfg 1> /dev/null
 #sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+
+# Set Hostname
+sudo hostnamectl set-hostname --static "fedora"
 
 # Plymouth fix
 #sudo plymouth-set-default-theme -R bgrt
 #sudo grubby --update-kernel=ALL --args=“plymouth.use-simpledrm”
 
-# Set Hostname
-sudo hostnamectl set-hostname --static "fedora"
+# GDM
+append_file "[daemon]
+AutomaticLogin=${user}
+AutomaticLoginEnable=True" /etc/gdm/custom.conf
 
-# Login/Display Manager
-if [ -d /etc/sddm.conf.d/ ];then
-  echo -e "[General]\nNumlock=on" | sudo tee /etc/sddm.conf.d/numlock.conf
-  echo -e "[Autologin]\nUser=${user}\nSession=sway.desktop" | sudo tee /etc/sddm.conf.d/autologin.conf
-else
-  echo -e "[daemon]\nAutomaticLogin=${user}\nAutomaticLoginEnable=True" | sudo tee -a /etc/gdm/custom.conf
+################################### THEMES ###################################
+
+# INSTALL: GTK, KDE, Icon, Cursors
+if [ ! -f /.flag ]; then
+	${source_dir}/themes/pack-libadwaita.sh
+	${source_dir}/themes/icon-tela-circle.sh
+	${source_dir}/themes/cursor-bibata.sh
 fi
