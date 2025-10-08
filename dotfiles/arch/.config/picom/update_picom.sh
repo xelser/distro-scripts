563e6cbf12bc78e62e7a87c2adfd01c845e8d0ba
xelser <dkzenzuri@gmail.com>
2025-10-09 02:34:10 +0800

    auto-update @ arch

--- dotfiles/arch/.config/picom/update_picom.sh
+++ dotfiles/arch/.config/picom/update_picom.sh
@@ -1,7 +1,11 @@
 #!/bin/env bash
 
 # revert to default and update
-cp -rf /etc/xdg/picom.conf $HOME/.config/picom/picom.conf
+if [ -f /etc/xdg/picom.conf.example ];then
+	cp -rf /etc/xdg/picom.conf.example $HOME/.config/picom/picom.conf
+else
+	cp -rf /etc/xdg/picom.conf $HOME/.config/picom/picom.conf
+fi
 
 # add user settings
 echo -e "@include \"overrides.conf\"" >> $HOME/.config/picom/picom.conf
