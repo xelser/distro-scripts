1b3373535bae3c776c7a370857ce19dabd56f678
xelser <dkzenzuri@gmail.com>
2025-10-17 01:27:42 +0800

    auto-update @ debian

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -15,7 +15,7 @@ apt install --yes build-essential htpdate dconf-cli libglib2.0-bin \
   fonts-roboto{,-slab} fonts-jetbrains-mono
 
 # INSTALL: WM (X11/Wayland)
-apt install --yes greetd gtkgreet seatd xinit xdg-desktop-portal-{gtk,wlr} \
+apt install --yes xinit seatd xdg-desktop-portal-{gtk,wlr} \
   brightnessctl dunst libnotify-bin mugshot at-spi2-core nwg-look mpv imv \
   transmission-gtk pavucontrol blueman lxpolkit engrampa pluma atril \
   thunar{,-archive-plugin} gvfs-{backends,fuse}
@@ -33,24 +33,6 @@ apt install --yes sway{bg,idle} waybar wlogout fuzzel grimshot wl-clipboard
 apt install --yes intel-media-va-driver-non-free libvpl2 libvpl-tools vainfo
 
 ################################### BUILD ####################################
-
-# overskride
-
-# xidlehook
-
-# wallutils
-#apt install --yes golang imagemagick libx11-dev libxcursor-dev libxmu-dev \
-#  libwayland-dev libxpm-dev xbitmaps libxmu-headers libheif-dev make
-#cd /tmp && git clone https://github.com/xyproto/wallutils
-#cd wallutils && make && sudo make PREFIX=/usr/local install
-
-# waytrogen
-#apt install --yes libgtk-4-1 openssl libsqlite3-0 libsqlite3-dev \
-#  libglib2.0-dev sqlite3 libgtk-4-dev meson ninja-build cargo
-
-#cd /tmp && git clone https://github.com/nikolaizombie1/waytrogen
-#cd waytrogen && meson setup builddir --prefix=/usr
-#meson compile -C builddir && meson install -C builddir
 
 # betterlockscreen
 apt install --yes autoconf gcc make pkg-config libpam0g-dev libcairo2-dev \
@@ -90,61 +72,17 @@ cd /tmp/swayfx-build/ && wget -q \
   && tar -xf "${swayfx_ver}.tar.gz" && cd swayfx-${swayfx_ver} && \
   meson build/ && ninja -C build/ && ninja -C build/ install
 
-################################### GREETD ###################################
-
-# main config
-cat > /etc/greetd/config.toml << EOF
-[terminal]
-vt = 7
-
-[default_session]
-command = "sway --config /etc/greetd/sway-config"
-user = "_greetd"
-
-[initial_session]
-command = "bash -l -c 'export DESKTOP_SESSION=sway XDG_CURRENT_DESKTOP=sway; exec sway'"
-user = "xelser"
-EOF
-
-# gtkgreet config
-cat > /etc/greetd/gtkgreet.css << EOF
-window {
-   background-image: url("file:///mnt/Home/Pictures/Wallpapers/Gruvbox/gruvbox_astro.jpg");
-   background-size: cover;
-   background-position: center;
-}
-
-box#body {
-   background-color: rgba(29, 32, 33, 0.5);
-   border-radius: 10px;
-   padding: 50px;
-}
-EOF
-
-# use sway for multi-monitor support
-cat > /etc/greetd/sway-config << EOF
-exec "gtkgreet -l -s /etc/greetd/gtkgreet.css; swaymsg exit"
-
-bindsym Mod4+shift+e exec swaynag \
-	-t warning \
-	-m 'What do you want to do?' \
-	-b 'Poweroff' 'systemctl poweroff' \
-	-b 'Reboot' 'systemctl reboot'
-
-include /etc/sway/config.d/*
-EOF
-
-# list of environments to login
-cat > /etc/greetd/environments << EOF
-startx
-bash
-sway
-EOF
-
 ################################### CONFIG ###################################
 
 # sudo
 usermod -aG sudo ${user}
+
+# autologin user at tty1
+mkdir -p /etc/systemd/system/getty@tty1.service.d
+echo "[Service]" > /etc/systemd/system/getty@tty1.service.d/override.conf
+echo "ExecStart=" >> /etc/systemd/system/getty@tty1.service.d/override.conf
+echo "ExecStart=-/sbin/agetty --autologin ${user} --noclear %I $TERM" >> /etc/systemd/system/getty@tty1.service.d/override.conf
+systemctl daemon-reexec
 
 # grub
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=30/g' /etc/default/grub
@@ -152,12 +90,6 @@ update-grub
 
 # swap/zram
 echo -e "[zram0]\nzram-size = ram / 2\ncompression-algorithm = zstd\nswap-priority = 100" > /etc/systemd/zram-generator.conf
-
-# sddm
-if [ -f /etc/sddm.conf ]; then
-  echo -e "[Autologin]\nUser=${user}\nSession=" >> /etc/sddm.conf
-  echo -e "\n[General]\nNumlock=on" >> /etc/sddm.conf
-fi
 
 # disable sleep/suspend/hibernate
 # systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
@@ -172,6 +104,21 @@ done
 # Fonts
 ${source_dir}/themes/fonts-nerd.sh JetBrainsMono
 
-# Wallpaper
-mkdir -p /usr/share/backgrounds
-ln -sf /mnt/Home/Pictures/Wallpapers/Gruvbox/ /usr/share/backgrounds/
+################################### OMITTED ##################################
+# overskride
+
+# xidlehook
+
+# wallutils
+#apt install --yes golang imagemagick libx11-dev libxcursor-dev libxmu-dev \
+#  libwayland-dev libxpm-dev xbitmaps libxmu-headers libheif-dev make
+#cd /tmp && git clone https://github.com/xyproto/wallutils
+#cd wallutils && make && sudo make PREFIX=/usr/local install
+
+# waytrogen
+#apt install --yes libgtk-4-1 openssl libsqlite3-0 libsqlite3-dev \
+#  libglib2.0-dev sqlite3 libgtk-4-dev meson ninja-build cargo
+
+#cd /tmp && git clone https://github.com/nikolaizombie1/waytrogen
+#cd waytrogen && meson setup builddir --prefix=/usr
+#meson compile -C builddir && meson install -C builddir
