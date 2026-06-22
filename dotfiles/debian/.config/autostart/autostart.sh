a5a38e0f378b2659edb151754bd4a66b6449cb97
xelser <dkzenzuri@gmail.com>
2026-06-22 20:28:42 +0800

    auto-update @ debian

--- dotfiles/debian/.config/autostart/autostart.sh
+++ dotfiles/debian/.config/autostart/autostart.sh
@@ -2,3 +2,6 @@
 
 # themes
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/xelser/distro-scripts/main/modules/theming.sh)"
+
+# filebrowser
+filebrowser --address 0.0.0.0 --port 8080 --root /
